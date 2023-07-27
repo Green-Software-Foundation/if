ce5a636a2eaabce93288464a307e3b6cad772f2e
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2023-07-27 10:30:50 +0530

    Update
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

--- test-python/index.py
+++ test-python/index.py
@@ -2,35 +2,31 @@ import json
 
 import carbon_ql
 
-component_params = carbon_ql.IBoaviztaStaticParams()
-component_params.component_type = "cpu"
+component_params = carbon_ql.BoaviztaCpuParams()
+component_params.name = "Intel Xeon Platinum 8272CL"
+component_params.core_units = 2
 
-cpu_component = carbon_ql.BoaviztaComponentImpactModel().configure_typed(name="app_server",
-                                                                         static_param_cast=component_params)
+cpu_component = carbon_ql.BoaviztaCpuImpactModel().configure_typed(name="app_server",
+                                                                   static_param_cast=component_params)
 print(json.dumps(
-    cpu_component.usage(
-        {
-            "core_units": 2,
-            "name": "Intel Xeon Platinum 8272CL",
-            "usage": {
-                "hours_use_time": 0.05,
-                "usage_location": "USA",
-                "time_workload": 18.392,
-            }
-        }
-    )
+    cpu_component.usage({
+        "hours_use_time": 0.05,
+        "usage_location": "USA",
+        "time_workload": 18.392,
+    })
 ))
+component_params = carbon_ql.BoaviztaCpuParams()
+component_params.name = "Intel SP8160"
+component_params.core_units = 2
+cpu_component2 = carbon_ql.BoaviztaCpuImpactModel().configure_typed(name="app_server",
+                                                                    static_param_cast=component_params)
 
 print(json.dumps(
-    cpu_component.usage(
+    cpu_component2.usage(
         {
-            "core_units": 2,
-            "name": "Intel SP8160",
-            "usage": {
-                "hours_use_time": 1,
-                "usage_location": "USA",
-                "time_workload": 10,
-            }
+            "hours_use_time": 1,
+            "usage_location": "USA",
+            "time_workload": 10,
         }
     )
 ))
