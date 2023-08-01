cccb9f68f26d7fc4144bd0c2da284695488bbc3b
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2023-08-01 10:46:30 +0530

    Standardize Instance metrics.
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@
 
 ccf-data:
 	cat third-party/ccf-coefficients/data/aws-instances.csv | python -c 'import csv, json, sys; print(json.dumps([dict(r) for r in csv.DictReader(sys.stdin)]))' > lib/ccf/aws-instances.json
+	cat third-party/ccf-coefficients/output/coefficients-aws-use.csv | python -c 'import csv, json, sys; print(json.dumps([dict(r) for r in csv.DictReader(sys.stdin)]))' > lib/ccf/aws-use.json
 	cat third-party/ccf-coefficients/output/coefficients-aws-embodied.csv | python -c 'import csv, json, sys; print(json.dumps([dict(r) for r in csv.DictReader(sys.stdin)]))' > lib/ccf/aws-embodied.json
 	cat third-party/ccf-coefficients/data/gcp-instances.csv | python -c 'import csv, json, sys; print(json.dumps([dict(r) for r in csv.DictReader(sys.stdin)]))' > lib/ccf/gcp-instances.json
 	cat third-party/ccf-coefficients/output/coefficients-gcp-use.csv | python -c 'import csv, json, sys; print(json.dumps([dict(r) for r in csv.DictReader(sys.stdin)]))' > lib/ccf/gcp-use.json
--- src/lib/ccf/index.ts
+++ src/lib/ccf/index.ts
@@ -1,12 +1,18 @@
 import {IImpactModelInterface} from "../interfaces";
 import * as aws_instances from './aws-instances.json';
 import * as gcp_instances from './gcp-instances.json';
+import * as gcp_use from './gcp-use.json';
+import * as aws_use from './aws-use.json';
+import * as azure_use from './azure-use.json';
 import * as azure_instances from './azure-instances.json';
 
 export class CloudCarbonFootprint implements IImpactModelInterface {
     authParams: object | undefined;
     name: string | undefined;
     computeInstances: { [key: string]: any } = {};
+    gcpList: { [key: string]: any } = {};
+    azureList: { [key: string]: any } = {};
+    awsList: { [key: string]: any } = {};
 
     constructor() {
         this.standardizeInstanceMetrics();
@@ -35,29 +41,70 @@ export class CloudCarbonFootprint implements IImpactModelInterface {
         this.computeInstances['aws'] = {};
         this.computeInstances['gcp'] = {};
         this.computeInstances['azure'] = {};
+        let gcpMin = 0.0;
+        let gcpMax = 0.0;
+        let gcpCount = 0;
+        gcp_use.forEach((instance: { [key: string]: any }) => {
+            this.gcpList[instance['Architecture']] = instance;
+        });
+        const gcpAvgMin = gcpMin / gcpCount;
+        const gcpAvgMax = gcpMax / gcpCount;
+        this.gcpList['Average'] = {
+            'Min Watts': gcpAvgMin,
+            'Max Watts': gcpAvgMax,
+            'Architecture': 'Average',
+        };
+        let azureMin = 0.0;
+        let azureMax = 0.0;
+        let azureCount = 0;
+        azure_use.forEach((instance: { [key: string]: any }) => {
+            this.azureList[instance['Architecture']] = instance;
+            azureMin += parseFloat(instance['Min Watts']);
+            azureMax += parseFloat(instance['Max Watts']);
+            azureCount += 1;
+        });
+        const azureAvgMin = azureMin / azureCount;
+        const azureAvgMax = azureMax / azureCount;
+        this.azureList['Average'] = {
+            'Min Watts': azureAvgMin,
+            'Max Watts': azureAvgMax,
+            'Architecture': 'Average',
+        }
+        aws_use.forEach((instance: { [key: string]: any }) => {
+            this.awsList[instance['Architecture']] = instance;
+        });
         aws_instances.forEach((instance: { [key: string]: any }) => {
             this.computeInstances['aws'][instance['Instance type']] = {
-                'instance_idle': instance['Instance @ Idle'],
-                'instance_10': instance['Instance @ 10%'],
-                'instance_50': instance['Instance @ 50%'],
-                'instance_100': instance['Instance @ 100%'],
-                'cpu': instance['Instance vCPU'],
+                'instance_idle': instance['Instance @ Idle'].replace(',', '.'),
+                'instance_10': instance['Instance @ 10%'].replace(',', '.'),
+                'instance_50': instance['Instance @ 50%'].replace(',', '.'),
+                'instance_100': instance['Instance @ 100%'].replace(',', '.'),
+                'vCPUs': instance['Instance vCPU'],
             };
         });
         gcp_instances.forEach((instance: { [key: string]: any }) => {
+            const cpus = parseInt(instance['Instance vCPUs'], 10);
+            let architecture = instance['Microarchitecture'];
+            if (!(architecture in this.azureList)) {
+                architecture = 'Average';
+            }
             this.computeInstances['gcp'][instance['Machine type']] = {
-                // 'instance_idle': instance['Instance @ Idle'],
-                // 'instance_10': instance['Instance @ 10%'],
-                // 'instance_50': instance['Instance @ 50%'],
-                // 'instance_100': instance['Instance @ 100%'],
-                'cpu': instance['Instance vCPU'],
+                'instance_min': this.gcpList[architecture]['Min Watts'] * cpus,
+                'instance_max': this.gcpList[architecture]['Max Watts'] * cpus,
+                'vCPUs': cpus
             };
-            console.log(instance);
         });
         azure_instances.forEach((instance: { [key: string]: any }) => {
-            if ('Instance Type' in instance) {
-
+            const cpus = parseInt(instance['Instance vCPUs'], 10);
+            let architecture = instance['Microarchitecture'];
+            if (!(architecture in this.azureList)) {
+                architecture = 'Average';
             }
+            this.computeInstances['azure'][instance['Virtual Machine']] = {
+                'instance_min': this.azureList[architecture]['Min Watts'] * cpus,
+                'instance_max': this.azureList[architecture]['Max Watts'] * cpus,
+                'cpu': instance['Instance vCPUs'],
+            };
         });
         console.log(this.computeInstances);
     }
