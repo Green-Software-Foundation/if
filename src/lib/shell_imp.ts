d57db060a6d56fe88f5101f073160451d72a390f
jmc <jmcook1186@gmail.com>
2023-08-30 16:48:30 +0100

    refine shell func

--- src/lib/shell_imp.ts
+++ src/lib/shell_imp.ts
@@ -2,47 +2,32 @@ const fs = require('fs');
 const yaml = require('js-yaml');
 const cp = require('child_process');
 
+/* 
+description:
+    spawns a child process to run an external IMP
+    expects execPath to be a path to an executable with a CLI exposing two methods: --calculate and --impl
+    The shell command then calls the --command method passing var impl as the path to the desired impl file
 
-function shellCommand(impl, execPath, omplName) {
-    /* 
-    description:
-        spawns a child process to run an external IMP
-        expects execPath to be a path to an executable with a CLI exposing two methods: --calculate and --impl
-        The shell command then calls the --command method passing var impl as the path to the desired impl file
+params:
+- impl: (string) path to impl file
+- execPath: (string) path to executable
+- omplName: (string) savename for ompl file
 
-    params:
-    - impl: (string) path to impl file
-    - execPath: (string) path to executable
-    - omplName: (string) savename for ompl file
-
-    returns:
-    - ompl data to stdout
-    - ompl data to disk as omplName.yaml
-    */
-
-    // create a child process to run the CLI
-    const exe = execPath; // assign the path to an executable
-    try {
-        const child = cp.spawn(exe, ['--calculate', '--impl=' + impl]); // pass the CLI argument in the array
-
-        // handle the return data
-        child.stdout.on('data', function (data) {
-            // print to stdout
-            var o = '' + data
-            const yamlStr = yaml.safeDump(o, { lineWidth: -1 });
-            console.log(yamlStr)
-            fs.writeFileSync(omplName, yamlStr, 'utf8');
-        });
-        // close the process
-        child.stdin.end();
-    }
-    catch (e) {
-        // report error to stderr
-        console.error("Error calling external IMP: please check input arguments");
-    }
+returns:
+- ompl data to stdout
+- ompl data to disk as omplName.yaml
+*/
+function runModelInShell(impl, execPath, omplName) {
+    var out = ''
+    const child = cp.spawn(execPath, ['--calculate', '--impl=' + impl]);
+    child.stdout.on('data', function (data) {
+        out = yaml.load(data)
+        fs.writeFileSync(omplName, yaml.dump(out), 'utf8');
+    });
+    child.stdin.end();
+    return out
 }
 
 //example invocation
 // calling prototype python model available in ief-sandbox repo
-
-//shellCommand('intel.yaml', '/bin/pimpl.py', 'ompl.yaml')
+let out = runModelInShell('dow_msft.yaml', '/home/joe/Code/ief-sandbox/dist/cli/cli', 'ompl2.yaml')
