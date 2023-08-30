0899fc1ce4450a015f36094589a4720198752680
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-08-30 18:11:58 +0400

    scripts: udate rimpl poc docs.
     - fixes #97

--- scripts/rimpl-poc.ts
+++ scripts/rimpl-poc.ts
@@ -2,15 +2,15 @@ import {parseProcessArgument} from '../src/util/args';
 import {openYamlFileAsObject, saveYamlFileAs} from '../src/util/yaml';
 
 /**
- * 1. Parses process argument.
+ * 1. Parses yml input/output process arguments.
  * 2. Opens yaml file as an object.
+ * 3. Saves processed object as an yaml file.
  * @todo Apply logic here.
  * @example run following command `npx ts-node scripts/rimpl-poc.ts --impl ./test.yml --ompl ./result.yml`
  */
 const rimplPOCScript = async () => {
   try {
     const {inputPath, outputPath} = parseProcessArgument();
-
     const impl = await openYamlFileAsObject(inputPath);
 
     if (!outputPath) {
