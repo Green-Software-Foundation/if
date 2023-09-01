fbe829f80b2a114a28fd26c6dbad1ed32f7c779e
Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>
2023-09-01 06:03:04 +0530

    Test fixes and adding interpolation
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>

--- src/lib/teads-aws/index.test.ts
+++ src/lib/teads-aws/index.test.ts
@@ -1,11 +1,11 @@
 import {describe, expect, jest, test} from '@jest/globals';
-import {TEADSEngineeringAWS} from './index';
+import {TeadsAWS} from './index';
 
 jest.setTimeout(30000);
 
 describe('teads:configure test', () => {
   test('initialize with params', async () => {
-    const impactModel = new TEADSEngineeringAWS();
+    const impactModel = new TeadsAWS();
     await impactModel.configure('test', {
       instance_type: 't2.micro',
     });
@@ -15,13 +15,13 @@ describe('teads:configure test', () => {
       ])
     ).resolves.toStrictEqual([
       {
-        e: 0.004900000000000001,
-        m: 0.04216723744292237 * 1000,
+        energy: 0.004900000000000001,
+        embodied: 0.04216723744292237 * 1000,
       },
     ]);
   });
   test('teads:initialize with params', async () => {
-    const impactModel = new TEADSEngineeringAWS();
+    const impactModel = new TeadsAWS();
     await impactModel.configure('test', {
       instance_type: 'm5n.large',
     });
@@ -45,16 +45,16 @@ describe('teads:configure test', () => {
       ])
     ).resolves.toStrictEqual([
       {
-        e: 0.0067,
-        m: 91.94006849315068,
+        energy: 0.0067,
+        embodied: 91.94006849315068,
       },
       {
-        e: 0.011800000000000001,
-        m: 91.94006849315068,
+        energy: 0.011800000000000001,
+        embodied: 91.94006849315068,
       },
       {
-        e: 0.016300000000000002,
-        m: 91.94006849315068,
+        energy: 0.016300000000000002,
+        embodied: 91.94006849315068,
       },
     ]);
   });
