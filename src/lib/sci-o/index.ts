c30243bab8ff3d4b40d746fcaf962249ef29b0cb
Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>
2023-09-04 11:27:31 +0530

    Complete implementation for sci-o
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>

--- src/lib/sci-o/index.ts
+++ src/lib/sci-o/index.ts
@@ -1,23 +1,44 @@
 import {IImpactModelInterface} from '../interfaces';
+import {KeyValuePair} from '../../types/boavizta';
 
 export class SciOModel implements IImpactModelInterface {
   authParams: object | undefined = undefined;
+  staticParams: object | undefined;
+  name: string | undefined;
   authenticate(authParams: object): void {
     this.authParams = authParams;
   }
 
   calculate(observations: object | object[] | undefined): Promise<object[]> {
-    return Promise.resolve([]);
+    if (!Array.isArray(observations)) {
+      throw new Error('observations should be an array');
+    }
+    observations.map((observation: KeyValuePair) => {
+      if ('grid-ci' in observation && 'energy' in observation) {
+        const grid_ci = parseFloat(observation['grid-ci']);
+        const energy = parseFloat(observation['energy']);
+        observation['operational-emissions'] = grid_ci * energy;
+      } else {
+        throw new Error(
+          'observation missing `grid-ci` or `energy`. Can not compute;'
+        );
+      }
+      return observation;
+    });
+
+    return Promise.resolve(observations);
   }
 
-  configure(
+  async configure(
     name: string,
     staticParams: object | undefined
   ): Promise<IImpactModelInterface> {
-    return Promise.resolve(undefined);
+    this.staticParams = staticParams;
+    this.name = name;
+    return this;
   }
 
   modelIdentifier(): string {
-    return '';
+    return 'org.gsf.sci-o';
   }
 }
