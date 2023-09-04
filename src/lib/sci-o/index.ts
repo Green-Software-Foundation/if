f84547428ba7cb260f6f19383c1e5ed4bcab75fa
Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>
2023-09-04 16:26:50 +0530

    Fixing error handling
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>

--- src/lib/sci-o/index.ts
+++ src/lib/sci-o/index.ts
@@ -5,6 +5,7 @@ export class SciOModel implements IImpactModelInterface {
   authParams: object | undefined = undefined;
   staticParams: object | undefined;
   name: string | undefined;
+
   authenticate(authParams: object): void {
     this.authParams = authParams;
   }
@@ -14,15 +15,15 @@ export class SciOModel implements IImpactModelInterface {
       throw new Error('observations should be an array');
     }
     observations.map((observation: KeyValuePair) => {
-      if ('grid-ci' in observation && 'energy' in observation) {
-        const grid_ci = parseFloat(observation['grid-ci']);
-        const energy = parseFloat(observation['energy']);
-        observation['operational-emissions'] = grid_ci * energy;
-      } else {
-        throw new Error(
-          'observation missing `grid-ci` or `energy`. Can not compute;'
-        );
+      if (!('grid-ci' in observation)) {
+        throw new Error('observation missing `grid-ci`');
       }
+      if (!('energy' in observation)) {
+        throw new Error('observation missing `energy`');
+      }
+      const grid_ci = parseFloat(observation['grid-ci']);
+      const energy = parseFloat(observation['energy']);
+      observation['operational-emissions'] = grid_ci * energy;
       return observation;
     });
 
