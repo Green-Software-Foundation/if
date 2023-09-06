7ec4ef1831bd0fa612fa5f1588eaba5c14315815
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-09-06 09:38:14 +0400

    util: implement models universe.

---
+++ src/util/models-universe.ts
@@ -0,0 +1,102 @@
+import {
+  BoaviztaCpuImpactModel,
+  BoaviztaCloudImpactModel,
+  // CloudCarbonFootprint,
+  ShellModel,
+  SciMModel,
+  SciOModel,
+  TeadsCurveModel,
+} from '../lib';
+
+import {
+  GraphOptions,
+  ImplInitializeModel,
+  InitalizedModels,
+} from '../types/models-universe';
+
+/**
+ * Models Initialization Lifecycle.
+ */
+export class ModelsUniverse {
+  /**
+   * Models list.
+   */
+  public initalizedModels: InitalizedModels = {};
+
+  /**
+   * Gets model class by provided `name` param.
+   */
+  private handBuiltinModel(name: string) {
+    switch (name) {
+      case 'boavizta-cpu':
+        return BoaviztaCpuImpactModel;
+      case 'boavizta-cloud':
+        return BoaviztaCloudImpactModel;
+      // case 'ccf':
+      //   return CloudCarbonFootprint;
+      case 'teads-curve':
+        return TeadsCurveModel;
+      case 'sci-m':
+        return SciMModel;
+      case 'sci-o':
+        return SciOModel;
+      default: // cover default
+        return BoaviztaCpuImpactModel;
+    }
+  }
+
+  /**
+   * Returns plugin model.
+   */
+  private handPluginModel() {
+    return ShellModel;
+  }
+
+  /**
+   * Returns shell model.
+   */
+  private handShellModel() {
+    return ShellModel;
+  }
+
+  /**
+   * Gets model based on `kind` and `name` params.
+   */
+  private handModelByCriteria(kind: string, name: string) {
+    switch (kind) {
+      case 'builtin':
+        return this.handBuiltinModel(name);
+      case 'plugin':
+        return this.handPluginModel();
+      case 'shell':
+        return this.handShellModel();
+      default: // cover default
+        return this.handBuiltinModel(name);
+    }
+  }
+
+  /**
+   * Initializes and registers model.
+   */
+  public writeDown(model: ImplInitializeModel) {
+    const {name, kind, config} = model;
+
+    const Model = this.handModelByCriteria(name, kind);
+    const callback = async (graphOptions: GraphOptions) => {
+      const params = {
+        ...config,
+        ...graphOptions,
+      };
+      const initalizedModel = await new Model().configure('test', params);
+
+      return initalizedModel;
+    };
+
+    this.initalizedModels = {
+      ...this.initalizedModels,
+      [name]: callback,
+    };
+
+    return this.initalizedModels;
+  }
+}
