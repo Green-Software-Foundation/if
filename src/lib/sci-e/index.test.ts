6a228790ce1c7d23489021296db2d91f45b01935
jmc <jmcook1186@gmail.com>
2023-09-12 17:43:07 +0100

    add sci-e model

---
+++ src/lib/sci-e/index.test.ts
@@ -0,0 +1,31 @@
+import {describe, expect, jest, test} from '@jest/globals';
+import {SciEModel} from './index';
+
+jest.setTimeout(30000);
+
+describe('teads:configure test', () => {
+  test('initialize with params', async () => {
+    const impactModel = new SciEModel();
+    await impactModel.configure('test', {});
+    await expect(
+      impactModel.calculate([
+        {
+          duration: 3600,
+          energy: 1,
+          e_net: 1,
+          e_mem: 1,
+          timestamp: '2021-01-01T00:00:00Z',
+        },
+      ])
+    ).resolves.toStrictEqual([
+      {
+        total_energy: 3,
+        duration: 3600,
+        energy: 1,
+        e_net: 1,
+        e_mem: 1,
+        timestamp: '2021-01-01T00:00:00Z',
+      },
+    ]);
+  });
+});
