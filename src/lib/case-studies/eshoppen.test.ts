3302596d23447b352d910994deb9376b658e6a49
Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>
2023-09-12 19:30:46 +0530

    Fix tests
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>

--- src/lib/case-studies/eshoppen.test.ts
+++ src/lib/case-studies/eshoppen.test.ts
@@ -1,41 +1,32 @@
 import {describe, expect, jest, test} from '@jest/globals';
 import {Eshoppen} from './eshoppen';
+
 jest.setTimeout(30000);
 
 describe('eshoppen:configure test', () => {
   test('initialize and test', async () => {
-    const model = await new Eshoppen().configure('eshoppen', {});
+    const model = await new Eshoppen().configure('eshoppen', {
+      type: 'e-cpu',
+    });
     expect(model).toBeInstanceOf(Eshoppen);
     await expect(
       model.calculate([
         {
+          'n-hours': 1,
+          'n-chips': 1,
+          tdp: 120,
+          'tdp-coeff': 1.02,
         },
       ])
     ).resolves.toStrictEqual([
       {
+        'e-cpu': 122.4,
+        'n-hours': 1,
+        'n-chips': 1,
+        tdp: 120,
+        'tdp-coeff': 1.02,
       },
     ]);
-    await expect(
-      model.calculate([
-        {
-          'grid-ci': 212.1,
-          energy: 100.0,
-        },
-      ])
-    ).resolves.toStrictEqual([
-      {
-        'grid-ci': 212.1,
-        energy: 100.0,
-        'operational-carbon': 100.0 * 212.1,
-      },
-    ]);
-    await expect(
-      model.calculate([
-        {
-          'grid-cid': 212.1,
-          energy: 100.0,
-        },
-      ])
-    ).rejects.toThrowError();
+    await expect(model.calculate([{}])).rejects.toThrowError();
   });
 });
