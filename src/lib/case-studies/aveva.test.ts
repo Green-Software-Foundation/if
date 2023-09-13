958a98e13a04618f67bb3880f56a926da4324ab8
jmc <jmcook1186@gmail.com>
2023-09-13 14:18:30 +0100

    yarn fix
    Signed-off-by: jmc <jmcook1186@gmail.com>

--- src/lib/case-studies/aveva.test.ts
+++ src/lib/case-studies/aveva.test.ts
@@ -1,5 +1,5 @@
-import { describe, expect, jest, test } from '@jest/globals';
-import { EAvevaModel } from './aveva-model';
+import {describe, expect, jest, test} from '@jest/globals';
+import {EAvevaModel} from './aveva-model';
 
 jest.setTimeout(30000);
 
