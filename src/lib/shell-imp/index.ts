afa7c93de28cc3011803864c8fb32c9bb749d521
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-09-15 17:08:42 +0400

    lib: restructure shell imp, imports, and comments.

--- src/lib/shell-imp/index.ts
+++ src/lib/shell-imp/index.ts
@@ -1,13 +1,18 @@
-import {IImpactModelInterface} from '../interfaces';
 import * as cp from 'child_process';
-import {KeyValuePair} from '../../types/boavizta';
 import * as yaml from 'js-yaml';
 
+import {IImpactModelInterface} from '../interfaces';
+
+import {CONFIG} from '../../config';
+
+import {KeyValuePair} from '../../types/common';
+
+const {MODEL_IDS} = CONFIG;
+const {SHELL_MODEL} = MODEL_IDS;
+
 export class ShellModel implements IImpactModelInterface {
-  // Defined for compatibility. Not used.
-  authParams: object | undefined;
-  // name of the data source
-  name: string | undefined;
+  authParams: object | undefined; // Defined for compatibility. Not used.
+  name: string | undefined; // The name of the data source.
   staticParams: object | undefined;
   executable = '';
 
@@ -19,10 +24,10 @@ export class ShellModel implements IImpactModelInterface {
   }
 
   /**
-   *  Configures the Plugin for IEF
-   *  @param {string} name name of the resource
-   *  @param {Object} staticParams static parameters for the resource
-   *  @param {number} staticParams.tdp Thermal Design Power in Watts
+   * Configures the Plugin for IEF
+   * @param {string} name name of the resource
+   * @param {Object} staticParams static parameters for the resource
+   * @param {number} staticParams.tdp Thermal Design Power in Watts
    */
   async configure(
     name: string,
@@ -62,30 +67,18 @@ export class ShellModel implements IImpactModelInterface {
    * Returns model identifier
    */
   modelIdentifier(): string {
-    return 'shellModel';
+    return SHELL_MODEL;
   }
 
-  /*
-  description:
-    spawns a child process to run an external IMP
-    expects execPath to be a path to an executable with a CLI exposing two methods: --calculate and --impl
-    The shell command then calls the --command method passing var impl as the path to the desired impl file
-
-  params:
-  - impl: yaml string (impl minus top level config)
-  - execPath: (string) path to executable
-  - omplName: (string) savename for ompl file
-
-  returns:
-  - ompl data to stdout
-  - ompl data to disk as omplName.yaml
-*/
   /**
-   * Runs the model in a shell
-   * @param input
-   * @param execPath
-   * @param omplName
-   * @private
+   * Runs the model in a shell. Spawns a child process to run an external IMP,
+   *  expects `execPath` to be a path to an executable with a CLI exposing two methods: `--calculate` and `--impl`.
+   * The shell command then calls the `--command` method passing var impl as the path to the desired impl file.
+   * @param input Yaml string (impl minus top level config).
+   * @param {string} execPath Path to executable.
+   * @param {string} omplName Savename for ompl file.
+   * @returns - ompl data to stdout
+   *          - ompl data to disk as omplName.yaml
    */
   private runModelInShell(input: string, execPath: string): KeyValuePair {
     try {
