f8bcbe640b5d16a8dfa85b9640a189b110c4fbcb
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-09-17 09:56:30 +0400

    config: add rimpl args, help message.

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,7 @@
+import {ArgumentConfig} from 'ts-command-line-args';
+
+import {RimplProcessArgs} from '../types/process-args';
+
 export const CONFIG = {
   MODEL_IDS: {
     BOAVIZTA_CPU: 'org.boavizta.cpu.sci',
@@ -19,13 +23,51 @@ export const CONFIG = {
     TEADS_CURVE: 'teads.curve',
     WATT_TIME: 'org.wattime.grid',
   },
-  VALIDATION: {
-    IMPL_CLI: {
-      impl: String,
+  RIMPL: {
+    ARGS: {
+      impl: {
+        type: String,
+        optional: true,
+        alias: 'i',
+        description: 'Path to an input IMPL file.',
+      },
       ompl: {
         type: String,
         optional: true,
+        description:
+          'Path to the output IMPL file where the results as saved, if none is provided it prints to stdout.',
       },
-    },
+      format: {
+        type: String,
+        optional: true,
+        description:
+          'The output file format. default to yaml but if csv is specified then it formats the impacts as a csv file for loading into another program.',
+        defaultValue: 'yaml',
+      },
+      verbose: {
+        type: Boolean,
+        optional: true,
+        description:
+          'How much information to output about the calculation to aid investigation and debugging.',
+      },
+      help: {
+        type: Boolean,
+        optional: true,
+        alias: 'h',
+        description: 'Prints this usage guide.',
+      },
+    } as ArgumentConfig<RimplProcessArgs>,
+    HELP: `rimpl 
+  -impl [path to the input impl file]
+  -ompl [path to the output impl file]
+  -format [yaml|csv] 
+  -verbose
+  -help 
+  impl: path to an input IMPL file
+  ompl: path to the output IMPL file where the results as saved, if none is provided it prints to stdout.
+  format: the output file format. default to yaml but if csv is specified then it formats the impacts as a csv file for loading into another program.
+  verbose: how much information to output about the calculation to aid investigation and debugging.
+  help: prints out the above help instruction.
+  `,
   },
 };
