0e8b3f81c1debf7db78ae92a80375bacc0bd6105
jmc <33655003+jmcook1186@users.noreply.github.com>
2023-09-20 14:55:43 +0100

    update sci model, sci mdoel tests, and implement in case study impls
    Signed-off-by: jmc <33655003+jmcook1186@users.noreply.github.com>

--- src/lib/sci/index.test.ts
+++ src/lib/sci/index.test.ts
@@ -1,12 +1,12 @@
-import {describe, expect, jest, test} from '@jest/globals';
-import {SciModel} from './index';
+import { describe, expect, jest, test } from '@jest/globals';
+import { SciModel } from './index';
 jest.setTimeout(30000);
 
 describe('sci:configure test', () => {
   test('initialize and test', async () => {
     const model = await new SciModel().configure('name', {
       time: 'minutes',
-      factor: 1,
+      functional_unit: 'users',
     });
     expect(model).toBeInstanceOf(SciModel);
     await expect(
@@ -14,13 +14,15 @@ describe('sci:configure test', () => {
         {
           'operational-carbon': 0.02,
           'embodied-carbon': 5,
+          'users': 100
         },
       ])
     ).resolves.toStrictEqual([
       {
         'operational-carbon': 0.02,
         'embodied-carbon': 5,
-        sci: 301.2,
+        'users': 100,
+        sci: 3.012,
       },
     ]);
     await expect(
@@ -28,20 +30,22 @@ describe('sci:configure test', () => {
         {
           'operational-carbon': 20,
           'embodied-carbon': 0.005,
+          users: 1000
         },
       ])
     ).resolves.toStrictEqual([
       {
         'operational-carbon': 20,
         'embodied-carbon': 0.005,
-        sci: 1200.3,
+        users: 1000,
+        sci: 1.2003,
       },
     ]);
   }),
     test('initialize and test', async () => {
       const model = await new SciModel().configure('name', {
         time: 'days',
-        factor: 100,
+        functionalUnit: ''
       });
       expect(model).toBeInstanceOf(SciModel);
       await expect(
@@ -55,7 +59,7 @@ describe('sci:configure test', () => {
         {
           'operational-carbon': 0.02,
           'embodied-carbon': 5,
-          sci: 4337.28,
+          sci: 433728,
         },
       ]);
       await expect(
@@ -69,7 +73,7 @@ describe('sci:configure test', () => {
         {
           'operational-carbon': 20,
           'embodied-carbon': 0.005,
-          sci: 17284.32,
+          sci: 1728432,
         },
       ]);
     });
--- src/lib/sci/index.ts
+++ src/lib/sci/index.ts
@@ -1,18 +1,18 @@
-import {IImpactModelInterface} from '../interfaces';
+import { IImpactModelInterface } from '../interfaces';
 
-import {CONFIG} from '../../config';
+import { CONFIG } from '../../config';
 
-import {KeyValuePair} from '../../types/common';
+import { KeyValuePair } from '../../types/common';
 
-const {MODEL_IDS} = CONFIG;
-const {SCI} = MODEL_IDS;
+const { MODEL_IDS } = CONFIG;
+const { SCI } = MODEL_IDS;
 
 export class SciModel implements IImpactModelInterface {
   authParams: object | undefined = undefined;
   staticParams: object | undefined;
   name: string | undefined;
   time: string | unknown;
-  factor = 1;
+  functionalUnit = 'none';
 
   authenticate(authParams: object): void {
     this.authParams = authParams;
@@ -37,34 +37,65 @@ export class SciModel implements IImpactModelInterface {
       let sci_timed: number = sci_secs;
 
       if (
+        this.time === 's' ||
         this.time === 'second' ||
         this.time === 'seconds' ||
-        this.time === ''
+        this.time === 'secs' ||
+        this.time === '' ||
+        this.time === null ||
+        this.time === "none"
+
       ) {
         sci_timed = sci_secs;
       }
-      if (this.time === 'minute' || this.time === 'minutes') {
+      if (this.time === 'minute' ||
+        this.time === 'minutes' ||
+        this.time === 'mins' ||
+        this.time === 'm'
+      ) {
         sci_timed = sci_secs * 60;
       }
-      if (this.time === 'hour' || this.time === 'hours') {
+      if (this.time === 'hour' ||
+        this.time === 'hours' ||
+        this.time === 'hr' ||
+        this.time === 'h'
+      ) {
         sci_timed = sci_secs * 60 * 60;
       }
-      if (this.time === 'day' || this.time === 'days') {
+      if (this.time === 'day' ||
+        this.time === 'days' ||
+        this.time === 'd'
+
+      ) {
         sci_timed = sci_secs * 60 * 60 * 24;
       }
-      if (this.time === 'week' || this.time === 'weeks') {
+      if (this.time === 'week' ||
+        this.time === 'weeks' ||
+        this.time === 'd'
+      ) {
         sci_timed = sci_secs * 60 * 60 * 24 * 7;
       }
       if (this.time === 'month' || this.time === 'months') {
         sci_timed = sci_secs * 60 * 60 * 24 * 7 * 4;
       }
-      if (this.time === 'year' || this.time === 'years') {
+      if (this.time === 'year' ||
+        this.time === 'years' ||
+        this.time === 'yr' ||
+        this.time === 'y') {
         sci_timed = sci_secs * 60 * 60 * 24 * 365;
       }
 
-      const factor = this.factor;
-      observation['sci'] = sci_timed / factor;
-      return observation;
+      const functionalUnit = this.functionalUnit;
+
+      if (this.functionalUnit != 'none') {
+        const factor = observation[functionalUnit];
+        observation['sci'] = sci_timed / factor;
+        return observation
+      } else {
+        observation['sci'] = sci_timed;
+        return observation
+      }
+
     });
 
     return tunedObservations;
@@ -84,8 +115,10 @@ export class SciModel implements IImpactModelInterface {
     if ('time' in staticParams) {
       this.time = staticParams?.time;
     }
-    if ('factor' in staticParams && typeof staticParams.factor === 'number') {
-      this.factor = staticParams?.factor;
+    if ('functional_unit' in staticParams && typeof staticParams.functional_unit === 'string') {
+      this.functionalUnit = staticParams?.functional_unit;
+    } else {
+      this.functionalUnit = 'none'
     }
 
     return this;
