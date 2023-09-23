ee602881e2637a4d86178b6a29e72a3276f5fc60
Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>
2023-09-24 05:14:24 +0530

    Update: Shell-Imp & TeadsAWS Model
    * Remove console logs in shell-imp
    * Add proper expected lifespan calculation on Teads aws model
    
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>

--- src/lib/teads-aws/index.test.ts
+++ src/lib/teads-aws/index.test.ts
@@ -126,4 +126,53 @@ describe('teads:configure test', () => {
       },
     ]);
   });
+  test('teads:initialize with params: linear 2', async () => {
+    const impactModel = new TeadsAWS();
+    await impactModel.configure('test', {
+      'instance-type': 'm5n.large',
+      interpolation: Interpolation.LINEAR,
+      'expected-lifespan': 8 * 365 * 24 * 3600,
+    });
+    await expect(
+      impactModel.calculate([
+        {
+          duration: 3600,
+          'cpu-util': 10,
+          timestamp: '2021-01-01T00:00:00Z',
+        },
+        {
+          duration: 3600,
+          'cpu-util': 50,
+          timestamp: '2021-01-01T00:00:00Z',
+        },
+        {
+          duration: 3600,
+          'cpu-util': 100,
+          timestamp: '2021-01-01T00:00:00Z',
+        },
+      ])
+    ).resolves.toStrictEqual([
+      {
+        duration: 3600,
+        'cpu-util': 10,
+        timestamp: '2021-01-01T00:00:00Z',
+        energy: 0.0067,
+        'embodied-carbon': 45.97003424657534,
+      },
+      {
+        duration: 3600,
+        'cpu-util': 50,
+        timestamp: '2021-01-01T00:00:00Z',
+        energy: 0.011800000000000001,
+        'embodied-carbon': 45.97003424657534,
+      },
+      {
+        duration: 3600,
+        'cpu-util': 100,
+        timestamp: '2021-01-01T00:00:00Z',
+        energy: 0.016300000000000002,
+        'embodied-carbon': 45.97003424657534,
+      },
+    ]);
+  });
 });
