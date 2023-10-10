d91fb18de94c5dceafe5372529c07f7892d7901c
Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>
2023-10-10 08:56:36 +0530

    Update: adding instances.json
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>

--- src/lib/cloud-instance-metadata/index.ts
+++ src/lib/cloud-instance-metadata/index.ts
@@ -3,6 +3,7 @@ import {IImpactModelInterface} from '../interfaces';
 import {CONFIG} from '../../config';
 
 import {KeyValuePair} from '../../types/common';
+import * as AWS_INSTANCES from './aws-instances.json';
 
 const {MODEL_IDS} = CONFIG;
 const {CLOUD_INSTANCE_METADATA} = MODEL_IDS;
@@ -29,6 +30,25 @@ export class CloudInstanceMetadataModel implements IImpactModelInterface {
     }
 
     return observations.map((observation: KeyValuePair) => {
+      let vendor = '';
+      let instance_type = '';
+      if ('cloud-vendor' in observation) {
+        vendor = observation['cloud-vendor'];
+      } else {
+        throw new Error('Each observation must contain a cloud-vendor key');
+      }
+      // if ('cloud-region' in observation) {
+      //   region = observation['cloud-region'];
+      // } else {
+      //   throw new Error('Each observation must contain a cloud-region key');
+      // }
+      if ('cloud-instance-type' in observation) {
+        instance_type = observation['cloud-instance-type'];
+      } else {
+        throw new Error(
+          'Each observation must contain a cloud-instance-type key'
+        );
+      }
       return observation;
     });
   }
