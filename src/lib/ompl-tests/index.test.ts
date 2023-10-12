695c37acb4f35fe941e72e5744f2335a37c56e05
jmc <33655003+jmcook1186@users.noreply.github.com>
2023-10-12 15:31:25 +0100

    tests: add test cases

--- src/lib/ompl-tests/index.test.ts
+++ src/lib/ompl-tests/index.test.ts
@@ -5,51 +5,33 @@ jest.setTimeout(30000);
 
 const path = "examples/ompls"
 var files = fs.readdirSync(path)
+
+if (files.length == 0) {
+    throw ("no ompl files available. Please run scripts/rimpl-test.sh before running these tests.")
+}
+
 files.forEach(function (file) {
     test('check ompls have impacts field', async () => {
         openYamlFileAsObject(path + "/" + file).then(ompl =>
             expect(ompl["graph"]["children"]["child"]["impacts"]))
     });
     if (file.includes("sci-m")) {
-        test('check sci-m has correct inputs and outputs', async () => {
+        test('check sci-m has correct output', async () => {
             openYamlFileAsObject(path + "/" + file).then(ompl =>
                 expect((ompl["graph"]["children"]["child"]["impacts"].includes("m"))));
         });
     }
-
+    if (file.includes("complex-pipeline")) {
+        test('check complex-pipeline has correct outputs', async () => {
+            openYamlFileAsObject(path + "/" + file).then(ompl => {
+                const res: string = ompl["graph"]["children"]["child"]["impacts"][0].toString();
+                expect((res.includes("e-mem")) &&
+                    (res.includes("e-cpu")) &&
+                    (res.includes("e-net")) &&
+                    (res.includes("energy")) &&
+                    (res.includes("operational-carbon"))
+                );
+            });
+        })
+    }
 })
-
-
-
-//if ompl["graph"]["children"]["child"]["pipeline"]
-
-
-// function runTests() {
-//     const path = "examples/ompls"
-//     fs.readdir(path, function (err, impls) {
-//         if (err) {
-//             console.error("Could not list the directory.", err);
-//             process.exit(1);
-//         }
-
-//         impls.forEach(function (file) {
-//             testOmpl(path, file)
-//         })
-//     }
-//     )
-// }
-
-// function testOmpl(path: string, file: string) {
-//     loadOmpl(path, file).then(ompl => {
-//         test("Check ompl has impacts field", () => {
-//             expect(ompl["graph"]["children"]["child"]["impacts"]);
-//         })
-//     })
-// }
-
-// async function loadOmpl(path: string, file: string) {
-//     const ompl = await openYamlFileAsObject(path + "/" + file);
-//     return ompl
-// }
-
-// runTests();
