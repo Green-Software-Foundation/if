c1298e71af8a838064ecfbc36fc3421e0f4a0873
jmc <33655003+jmcook1186@users.noreply.github.com>
2023-10-12 15:33:43 +0100

    tests: run linter

--- src/lib/ompl-tests/index.test.ts
+++ src/lib/ompl-tests/index.test.ts
@@ -1,42 +1,46 @@
-import { openYamlFileAsObject } from "../../util/yaml";
-import { expect, jest, test } from '@jest/globals';
-import * as fs from "fs";
+import {openYamlFileAsObject} from '../../util/yaml';
+import {expect, jest, test} from '@jest/globals';
+import * as fs from 'fs';
 jest.setTimeout(30000);
 
-const path = "examples/ompls"
-var files = fs.readdirSync(path)
+const path = 'examples/ompls';
+const files = fs.readdirSync(path);
 /*
 If there are no files in examples/ompls, prompt user to run ompl-test bash script
 */
 if (files.length == 0) {
-    throw ("no ompl files available. Please run scripts/rimpl-test.sh before running these tests.")
+  throw 'no ompl files available. Please run scripts/rimpl-test.sh before running these tests.';
 }
 
 /*
 For each file in examples/ompls, load it and run test
 */
-files.forEach(function (file) {
-    test('check ompls have impacts field', async () => {
-        openYamlFileAsObject(path + "/" + file).then(ompl =>
-            expect(ompl["graph"]["children"]["child"]["impacts"]))
+files.forEach(file => {
+  test('check ompls have impacts field', async () => {
+    openYamlFileAsObject(path + '/' + file).then(ompl =>
+      expect(ompl['graph']['children']['child']['impacts'])
+    );
+  });
+  if (file.includes('sci-m')) {
+    test('check sci-m has correct output', async () => {
+      openYamlFileAsObject(path + '/' + file).then(ompl =>
+        expect(ompl['graph']['children']['child']['impacts'].includes('m'))
+      );
     });
-    if (file.includes("sci-m")) {
-        test('check sci-m has correct output', async () => {
-            openYamlFileAsObject(path + "/" + file).then(ompl =>
-                expect((ompl["graph"]["children"]["child"]["impacts"].includes("m"))));
-        });
-    }
-    if (file.includes("complex-pipeline")) {
-        test('check complex-pipeline has correct outputs', async () => {
-            openYamlFileAsObject(path + "/" + file).then(ompl => {
-                const res: string = ompl["graph"]["children"]["child"]["impacts"][0].toString();
-                expect((res.includes("e-mem")) &&
-                    (res.includes("e-cpu")) &&
-                    (res.includes("e-net")) &&
-                    (res.includes("energy")) &&
-                    (res.includes("operational-carbon"))
-                );
-            });
-        })
-    }
-})
+  }
+  if (file.includes('complex-pipeline')) {
+    test('check complex-pipeline has correct outputs', async () => {
+      openYamlFileAsObject(path + '/' + file).then(ompl => {
+        const res: string =
+          ompl['graph']['children']['child']['impacts'][0].toString();
+        expect(
+          res.includes('e-mem') &&
+            res.includes('e-cpu') &&
+            res.includes('e-net') &&
+            res.includes('energy') &&
+            res.includes('operational-carbon')
+        );
+      });
+    });
+  }
+});
