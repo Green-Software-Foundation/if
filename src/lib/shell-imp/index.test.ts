ae494ee67890ce67248b80b0f5d5a18953aecc70
jmc <33655003+jmcook1186@users.noreply.github.com>
2023-10-16 10:45:47 +0100

    [models]: fix test

--- src/lib/shell-imp/index.test.ts
+++ src/lib/shell-imp/index.test.ts
@@ -1,5 +1,5 @@
-import {describe, expect, jest, test} from '@jest/globals';
-import {ShellModel} from './index';
+import { describe, expect, jest, test } from '@jest/globals';
+import { ShellModel } from './index';
 
 jest.setTimeout(30000);
 
@@ -13,10 +13,10 @@ describe('shell:configure test', () => {
     });
     await expect(
       impactModel.calculate([
-        {duration: 3600, cpu: 0.5, datetime: '2021-01-01T00:00:00Z'},
+        { duration: 3600, cpu: 0.5, datetime: '2021-01-01T00:00:00Z' },
       ])
     ).resolves.toStrictEqual([
-      {duration: 3600, cpu: 0.5, datetime: '2021-01-01T00:00:00Z', e: 1},
+      { duration: 3600, cpu: 0.5, datetime: '2021-01-01T00:00:00Z', energy: 1 },
     ]);
   });
 });
