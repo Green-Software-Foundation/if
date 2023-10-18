634668ee3b8f7b26d99d21e4c8ea5faf81d8b1da
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-10-18 12:53:45 +0400

    tests: resolve shell imp tests.

--- src/__tests__/unit/lib/shell-imp/index.test.ts
+++ src/__tests__/unit/lib/shell-imp/index.test.ts
@@ -3,20 +3,22 @@ import {ShellModel} from '../../../../lib/shell-imp/index';
 
 jest.setTimeout(30000);
 
-describe.skip('shell:configure test', () => {
-  test('initialize with params', async () => {
-    const impactModel = new ShellModel();
-    await impactModel.configure('test', {
-      provider: 'aws',
-      instance_type: 't2.micro',
-      executable: '/usr/local/bin/sampler',
+describe('lib/shell-imp: ', () => {
+  describe('shell:configure', () => {
+    test('initialize with params', async () => {
+      const impactModel = new ShellModel();
+      await impactModel.configure('test', {
+        provider: 'aws',
+        instance_type: 't2.micro',
+        executable: '/usr/local/bin/sampler',
+      });
+      await expect(
+        impactModel.calculate([
+          {duration: 3600, cpu: 0.5, datetime: '2021-01-01T00:00:00Z'},
+        ])
+      ).resolves.toStrictEqual([
+        {duration: 3600, cpu: 0.5, datetime: '2021-01-01T00:00:00Z', energy: 1},
+      ]);
     });
-    await expect(
-      impactModel.calculate([
-        {duration: 3600, cpu: 0.5, datetime: '2021-01-01T00:00:00Z'},
-      ])
-    ).resolves.toStrictEqual([
-      {duration: 3600, cpu: 0.5, datetime: '2021-01-01T00:00:00Z', energy: 1},
-    ]);
   });
 });
