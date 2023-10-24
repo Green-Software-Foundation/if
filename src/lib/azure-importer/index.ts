8de877924a9af9de266e4e517fc5c36ed109c20a
jmc <33655003+jmcook1186@users.noreply.github.com>
2023-10-24 15:10:40 +0100

    models: add simple json parsing

--- src/lib/azure-importer/index.ts
+++ src/lib/azure-importer/index.ts
@@ -2,7 +2,7 @@ import { IOutputModelInterface } from '../interfaces';
 
 import { CONFIG } from '../../config';
 
-// import { KeyValuePair } from '../../types/common';
+//import { KeyValuePair } from '../../types/common';
 
 const { MODEL_IDS } = CONFIG;
 const { AZURE_IMPORTER } = MODEL_IDS;
@@ -37,40 +37,52 @@ export class AzureImporterModel implements IImpactModelInterface {
      */
     async calculate(observations: object | object[] | undefined): Promise<any[]> {
         if (observations === undefined) {
-            observations = ['something', 'something else'];
-            throw new Error('Required Parameters not provided');
+            throw new Error('Observatons missing: Please create an observations field in your IMPL file');
         } else if (!Array.isArray(observations)) {
             throw new Error('Observations must be an array');
         }
-        console.log('in here, doing the thing');
-        return observations;
 
-        // return observations.map((observation: KeyValuePair) => {
-        //     if (!('grid-carbon-intensity' in observation)) {
-        //         throw new Error('observation missing `grid-carbon-intensity`');
-        //     }
-        //     if (!('energy' in observation)) {
-        //         throw new Error('observation missing `energy`');
-        //     }
-        //     this.configure(this.name!, observation);
-        //     const grid_ci = parseFloat(observation['grid-carbon-intensity']);
-        //     const energy = parseFloat(observation['energy']);
-        //     observation['operational-carbon'] = grid_ci * energy;
-        //     return observation;
-        // });
-    }
+        // dummy data that will eventually come from API response
+        var data = [{ "timeStamp": "2023-10-24T11:24:00Z", "Percentage CPU": 35 }, { "timeStamp": "2023-10-24T11:25:00Z", "Percentage CPU": 45 }];
+
+        for (var i = 0; i < data.length; i++) {
+            for (var entry of data) {
+                observations.push({ 'timestamp': entry['timeStamp'] }, { 'cpu-util': entry['Percentage CPU'] })
+            }
+        }
+        return observations
+
+        // IN HERE RECEIVE AZURE RESPONSE DATA AND REFORMAT FOR IMPL
+        // INSERT INTO IMPL AS OBSERVATIONS
+
+    };
+
+    // return observations.map((observation: KeyValuePair) => {
+    //     if (!('grid-carbon-intensity' in observation)) {
+    //         throw new Error('observation missing `grid-carbon-intensity`');
+    //     }
+    //     if (!('energy' in observation)) {
+    //         throw new Error('observation missing `energy`');
+    //     }
+    //     this.configure(this.name!, observation);
+    //     const grid_ci = parseFloat(observation['grid-carbon-intensity']);
+    //     const energy = parseFloat(observation['energy']);
+    //     observation['operational-carbon'] = grid_ci * energy;
+    //     return observation;
+    // });
+}
 
     async configure(
-        name: string,
-        staticParams: object | undefined
-    ): Promise<IImpactModelInterface> {
-        this.staticParams = staticParams;
-        this.name = name;
+    name: string,
+    staticParams: object | undefined
+): Promise < IImpactModelInterface > {
+    this.staticParams = staticParams;
+    this.name = name;
 
-        return this;
-    }
+    return this;
+}
 
-    modelIdentifier(): string {
-        return AZURE_IMPORTER;
-    }
+modelIdentifier(): string {
+    return AZURE_IMPORTER;
+}
 }
