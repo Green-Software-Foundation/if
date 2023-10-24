b25393cc588e2512e86dfa86464b715b6cc7ee16
jmc <33655003+jmcook1186@users.noreply.github.com>
2023-10-24 13:51:03 +0100

    add skeleton Azure model

--- src/lib/azure-importer/index.ts
+++ src/lib/azure-importer/index.ts
@@ -14,25 +14,22 @@ export class AzureImporterModel implements IImpactModelInterface {
 
     authenticate(authParams: object): void {
         this.authParams = authParams;
+
     }
 
+    // async getAuth(){
+    // ADD A FUNCTION TO GET THE NECESSARY AUTH FOR REQUESTING AZURE METRICS
+    // DATA SUCH AS CLIENT-SECRET, RESOURCE-ID, TENTANT-ID etc SHOULD BE IN .ENV
+    // USE "dotenv" LIB TO IMPORT
+    //}
 
-    /**
-     * CONFIGURE NEEDS TO FIND ALL THE REQUEST PARAMS, INCL METRIC TYPE, AGGREGATION, INTERVAL
-     * 
-     * PLAN TO ADD:
-     *  AUTH() check env vars
-     *  REQUEST()
-     *  MARSHALL()
-     *  as private functions to be called in calculate()
-     * 
-     *  REQUEST() will have to pull some info from .env.local to authorize API request to Azure
-     *  MARSHALL will have to parse json response and reorganize into yaml/observations
-     */
+    // async fetchData() {
+    // ADD A FUNCTION TO MAKE THE HTTP GET REQUEST THAT RETURNS CPU-UTIL AN TIMESTAMP
+    // QUERY PARAMS WILL COME FROM IMPL AS STATICPARAMS
+    // RETURNS JSON RESPONSE DATA
+    //}
 
-
-
-
+    // 
 
     /**
      * Calculate the total emissions for a list of observations.
@@ -41,29 +38,18 @@ export class AzureImporterModel implements IImpactModelInterface {
      * @param {Object[]} observations
      */
     async calculate(observations: object | object[] | undefined): Promise<any[]> {
+        // IN HERE RECEIVE AZURE RESPONSE DATA AND REFORMAT FOR IMPL
+        // INSERT INTO IMPL AS OBSERVATIONS
+
         if (observations === undefined) {
             observations = ['something', 'something else'];
             throw new Error('Required Parameters not provided');
         } else if (!Array.isArray(observations)) {
             throw new Error('Observations must be an array');
         }
-        console.log("in here, doing the thing");
         return observations
+    };
 
-        // return observations.map((observation: KeyValuePair) => {
-        //     if (!('grid-carbon-intensity' in observation)) {
-        //         throw new Error('observation missing `grid-carbon-intensity`');
-        //     }
-        //     if (!('energy' in observation)) {
-        //         throw new Error('observation missing `energy`');
-        //     }
-        //     this.configure(this.name!, observation);
-        //     const grid_ci = parseFloat(observation['grid-carbon-intensity']);
-        //     const energy = parseFloat(observation['energy']);
-        //     observation['operational-carbon'] = grid_ci * energy;
-        //     return observation;
-        // });
-    }
 
     async configure(
         name: string,
