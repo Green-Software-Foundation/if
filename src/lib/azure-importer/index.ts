4ca66e77eb243acc95a7f71f4a39de427b3ee656
Yassine El Ghali <Yassine.el.ghali@microsoft.com>
2023-10-26 00:24:00 +0200

    adding function getVMUsage to get azure VM CPU & RAM from azure monitor

--- src/lib/azure-importer/index.ts
+++ src/lib/azure-importer/index.ts
@@ -4,6 +4,72 @@ import { CONFIG } from '../../config';
 
 //import { KeyValuePair } from '../../types/common';
 
+import { DefaultAzureCredential } from "@azure/identity";
+import { MonitorClient } from "@azure/arm-monitor";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+
+//Azure Model Config params
+const mysubscriptionId = "0f4bda7e-1203-4f11-9a85-22653e9af4b4"
+const myRG = "green"
+const myVM = "test1"
+
+
+async function getVmUsage() {
+    const subscriptionId = mysubscriptionId;
+    const resourceGroupName = myRG;
+    const vmName = myVM;
+
+  // Use DefaultAzureCredential which works with AZURE_TENANT_ID, AZURE_CLIENT_ID, and AZURE_CLIENT_SECRET environment variables.
+  // You can also use other credentials provided by @azure/identity package.
+  const credential = new DefaultAzureCredential();
+
+  const monitorClient = new MonitorClient(credential, subscriptionId);
+
+  const cpuMetricsResponse = await monitorClient.metrics.list(
+    `subscriptions/${subscriptionId}/resourceGroups/${resourceGroupName}/providers/Microsoft.Compute/virtualMachines/${vmName}`,
+    {
+      metricnames: "Percentage CPU",
+      timespan: "PT1H", // last one hour
+      interval: "PT1M", // granularity of one minute
+      aggregation: "Average",
+    }
+  );
+
+  console.log("CPU Usage:");
+  for (const timeSeries of cpuMetricsResponse.value[0].timeseries || []) {
+    for (const data of timeSeries.data || []) {
+      console.log(`Time: ${data.timeStamp}, CPU usage: ${data.average}`);
+    }
+  }
+
+  const ramMetricsResponse = await monitorClient.metrics.list(
+    `subscriptions/${subscriptionId}/resourceGroups/${resourceGroupName}/providers/Microsoft.Compute/virtualMachines/${vmName}`,
+    {
+      metricnames: "Available Memory Bytes", // TODO: we need to get memory used = total - available
+      timespan: "PT1H", // last one hour
+      interval: "PT1M", // granularity of one minute
+      aggregation: "Average",
+    }
+  );
+
+  console.log("RAM Usage:");
+  for (const timeSeries of ramMetricsResponse.value[0].timeseries || []) {
+    for (const data of timeSeries.data || []) {
+      console.log(`Time: ${data.timeStamp}, RAM usage: ${data.average}`);
+    }
+  }
+}
+
+// Call the function
+getVmUsage().catch((err) => {
+  console.error("An error occurred:", err);
+});
+
+
+
 const { MODEL_IDS } = CONFIG;
 const { AZURE_IMPORTER } = MODEL_IDS;
 
