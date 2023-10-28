b810740efcfe36375bd0d3a2dbe263fdf2e7fc58
Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>
2023-10-28 15:43:03 +0530

    Impact importer interface
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>

--- src/lib/interfaces/index.ts
+++ src/lib/interfaces/index.ts
@@ -1,14 +1,21 @@
-export interface IImpactModelInterface {
+export interface IImpactBaseInterface {
   modelIdentifier(): string;
 
   // params is a reserved keyword in C#. Hence it can not be used.
   configure(
     name: string,
     staticParams: object | undefined
-  ): Promise<IImpactModelInterface>;
+  ): Promise<IImpactBaseInterface>;
 
   authenticate(authParams: object): void;
+}
 
+export interface IImpactModelInterface extends IImpactBaseInterface {
   calculate(observations: object | object[] | undefined): Promise<any[]>;
 }
+
+export interface IImpactImporterInterface extends IImpactBaseInterface {
+  importObservations(): Promise<any[]>;
+}
+
 export * from './ccf';
