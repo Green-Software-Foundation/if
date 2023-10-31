f76a23cd4377d8ef280c58511429fb5f9fb32138
jmc <33655003+jmcook1186@users.noreply.github.com>
2023-10-31 15:17:03 +0000

    docs: run linter

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -74,7 +74,7 @@ const config = {
             position: 'left',
             label: 'Docs',
           },
-          { to: '/blog', label: 'Blog', position: 'left' },
+          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/Green-Software-Foundation/ief',
             label: 'GitHub',
