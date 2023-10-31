1785a84f448315b62947178b57edfb34284df8a5
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-10-31 10:03:29 +0400

    config: init error messages for models universe.

--- src/config/strings.ts
+++ src/config/strings.ts
@@ -1,3 +1,10 @@
 export const STRINGS = {
   WRONG_CLI_ARGUMENT: 'Wrong or missing argument.',
+  MISSING_CLASSNAME: 'Model classname is missing.',
+  NOT_INPUT_MODEL_EXTENSION:
+    'Provided model does not extend IImpactModelInterface.',
+  NOT_INITIALIZED_MODEL: (modelName: string) =>
+    `Model ${modelName} is not initalized yet.`,
+  WRONG_OR_MISSING_MODEL: (modelName: string) =>
+    `Missing or wrong model: ${modelName}.`,
 };
