d8823de3953539a76a8300f95c12964c58196bad
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-11-01 18:47:22 +0400

    config: tune strings to provide updated interface.

--- src/config/strings.ts
+++ src/config/strings.ts
@@ -1,8 +1,8 @@
 export const STRINGS = {
   WRONG_CLI_ARGUMENT: 'Wrong or missing argument.',
   MISSING_CLASSNAME: 'Model classname is missing.',
-  NOT_INPUT_MODEL_EXTENSION:
-    'Provided model does not extend IImpactModelInterface.',
+  NOT_OUTPUT_MODEL_EXTENSION:
+    'Provided model does not extend IOutputModelInterface.',
   NOT_INITIALIZED_MODEL: (modelName: string) =>
     `Model ${modelName} is not initalized yet.`,
   WRONG_OR_MISSING_MODEL: (modelName: string) =>
