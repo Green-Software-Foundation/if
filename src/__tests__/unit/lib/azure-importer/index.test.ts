cfaca7b18a8d4036b9ad5b64bf64ac6b8ac6db62
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-11-03 20:17:48 +0400

    tests: initialize testing structure for azure  importer.

--- src/__tests__/unit/lib/azure-importer/index.test.ts
+++ src/__tests__/unit/lib/azure-importer/index.test.ts
@@ -1,21 +1,52 @@
-import {describe, expect, jest, test} from '@jest/globals';
-import {AzureImporterModel} from '../../../../lib/azure-importer/index';
+import {AzureImporterModel} from '../../../../lib/azure-importer';
+
 jest.setTimeout(30000);
 
-describe('sci:configure test', () => {
-  test('initialize and test', async () => {
-    const model = await new AzureImporterModel().configure('name', {});
-    expect(model).toBeInstanceOf(AzureImporterModel);
-    await expect(
-      model.execute([
-        {
-          duration: 3600,
-          timestamp: '2023-11-01T14:45:00Z',
-          interval: 'PT1M',
-          timespan: 'PT1H',
-          aggregation: 'average',
-        },
-      ])
-    ).resolves.toStrictEqual(['dummy']);
+describe('lib/azure-importer: ', () => {
+  describe('init AzureImporterModel: ', () => {
+    it('initalizes object with properties.', async () => {
+      const model = await new AzureImporterModel();
+
+      expect(model).toHaveProperty('authenticate');
+      expect(model).toHaveProperty('configure');
+      expect(model).toHaveProperty('execute');
+      expect(model).toHaveProperty('modelIdentifier');
+
+      expect(model).toHaveProperty('name');
+      expect(model).toHaveProperty('authParams');
+      expect(model).toHaveProperty('staticParams');
+    });
+
+    it('initializes object and test', async () => {
+      const model = await new AzureImporterModel().configure('name', {});
+      expect(model).toBeInstanceOf(AzureImporterModel);
+      await expect(
+        model.execute([
+          {
+            duration: 3600,
+            timestamp: '2023-11-01T14:45:00Z',
+            interval: 'PT1M',
+            timespan: 'PT1H',
+            aggregation: 'average',
+          },
+        ])
+      ).resolves.toStrictEqual(['dummy']);
+    });
+  });
+
+  describe('authenticate(): ', () => {
+    it('');
+  });
+
+  describe('configure(): ', () => {
+    it('');
+  });
+
+  describe('execute(): ', () => {
+    it('');
+  });
+
+  describe('modelIdentifier(): ', () => {
+    it('');
   });
 });
