2f67daf12885e505ec95bb59b96443017bb8cc3d
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-11-08 18:34:43 +0400

    types: init impl dtos.

---
+++ src/types/impl.ts
@@ -0,0 +1,42 @@
+type Tag = {
+  kind?: string;
+  complexity?: string;
+  category?: string;
+};
+
+type Model = {
+  name: string;
+  kind: string;
+  verbose?: boolean;
+  path?: string;
+};
+
+export type ModelParams = {
+  timestamp: number;
+  duration: number;
+  [key: string]: any;
+};
+
+export type Config = {[key: string]: any};
+
+export type Children = {
+  [key: string]: {
+    pipeline: string[];
+    config: Config;
+    inputs: ModelParams[];
+    children?: Children;
+    outputs?: ModelParams[];
+  };
+};
+
+export type Impl = {
+  name: string;
+  description: string;
+  tags: Tag | null | undefined;
+  initialize: {
+    models: Model[];
+  };
+  graph: {
+    children: Children;
+  };
+};
