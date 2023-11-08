f5d79ef3c64148b0492815b68b7ac808efdae81d
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-11-08 10:41:25 +0400

    config: in strings improve error messages.

--- src/config/strings.ts
+++ src/config/strings.ts
@@ -1,11 +1,11 @@
 export const STRINGS = {
-  WRONG_CLI_ARGUMENT: 'Wrong or missing argument.',
-  MISSING_CLASSNAME: 'Model classname is missing.',
-  NOT_OUTPUT_MODEL_EXTENSION:
-    'Provided model does not extend IOutputModelInterface.',
+  FILE_IS_NOT_YAML: 'Provided impl file is not in yaml format.',
+  IMPL_IS_MISSING: 'Impl file is missing.',
+  MISSING_CLASSNAME: 'Initalization param `model` is missing.',
+  MISSING_PATH: 'Initalization param `path` is missing.',
+  NOT_OUTPUT_MODEL_EXTENSION: 'Provided model does not extend ModelInterface.',
   NOT_INITIALIZED_MODEL: (modelName: string) =>
     `Model ${modelName} is not initalized yet.`,
   WRONG_OR_MISSING_MODEL: (modelName: string) =>
-    `Missing or wrong model: ${modelName}.`,
-  MISSING_PATH: 'Plugin path parameter is missing.',
+    `You are trying to configure a built-in model that is not recognized: ${modelName}.`,
 };
