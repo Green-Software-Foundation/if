e31dbcaf709657cf4826c825fd78ddb8e91ec2e8
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-11-08 10:40:52 +0400

    util: implement errors layer.

---
+++ src/util/errors.ts
@@ -0,0 +1,24 @@
+const CUSTOM_ERRORS = [
+  'CliInputError',
+  'ImplValidationError',
+  'ModelInitializationError',
+  'ModelCredentialError',
+] as const;
+
+type CustomErrors = {
+  [K in (typeof CUSTOM_ERRORS)[number]]: ErrorConstructor;
+};
+
+export const ERRORS = CUSTOM_ERRORS.reduce((acc, className) => {
+  acc = {
+    ...acc,
+    [className]: class extends Error {
+      constructor(message: string) {
+        super(message);
+        this.name = this.constructor.name;
+      }
+    },
+  };
+
+  return acc;
+}, {} as CustomErrors);
