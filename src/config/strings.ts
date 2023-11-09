d8dbd89b272e87dbad6b3d117015f65f9c7df6a0
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-11-09 20:22:27 +0400

    config: remove wrong or missing model message.

--- src/config/strings.ts
+++ src/config/strings.ts
@@ -7,6 +7,4 @@ export const STRINGS = {
   NOT_OUTPUT_MODEL_EXTENSION: 'Provided model does not extend ModelInterface.',
   NOT_INITIALIZED_MODEL: (modelName: string) =>
     `Model ${modelName} is not initalized yet.`,
-  WRONG_OR_MISSING_MODEL: (modelName: string) =>
-    `You are trying to configure a built-in model that is not recognized: ${modelName}.`,
 };
