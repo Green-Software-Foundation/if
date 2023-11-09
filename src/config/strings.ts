dc4db7f12b549dd0dbb1b5fc6cb1ec2162a9246a
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-11-09 14:30:56 +0400

    config: add model not supported message.

--- src/config/strings.ts
+++ src/config/strings.ts
@@ -3,6 +3,7 @@ export const STRINGS = {
   IMPL_IS_MISSING: 'Impl file is missing.',
   MISSING_CLASSNAME: 'Initalization param `model` is missing.',
   MISSING_PATH: 'Initalization param `path` is missing.',
+  MODEL_DOESNT_EXIST: 'Provided model class does not belong to any model.',
   NOT_OUTPUT_MODEL_EXTENSION: 'Provided model does not extend ModelInterface.',
   NOT_INITIALIZED_MODEL: (modelName: string) =>
     `Model ${modelName} is not initalized yet.`,
