8b61b29fabae183d73b841312ac192f8861e0a40
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-11-10 11:04:15 +0400

    config: add disclaimer message to strings.

--- src/config/strings.ts
+++ src/config/strings.ts
@@ -9,4 +9,10 @@ export const STRINGS = {
     `Graph is malformed: graph.children.${childName} is not valid.`,
   NOT_INITIALIZED_MODEL: (modelName: string) =>
     `Model ${modelName} is not initalized yet.`,
+  DISCLAIMER_MESSAGE: `
+[!important] Incubation Project
+
+This project is an incubation project being run inside the Green Software Foundation; as such, we *DON’T recommend using it in any critical use case. 
+Incubation projects are experimental, offer no support guarantee, have minimal governance and process, and may be retired at any moment. This project may one day graduate, in which case this disclaimer will be removed.
+`,
 };
