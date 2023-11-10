02664a8a1544f7112b70876221885a6111351dbc
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-11-10 20:19:36 +0400

    util: simplify error helper.

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -7,12 +7,12 @@ const {ISSUE_TEMPLATE} = STRINGS;
 /**
  * Impact engine error handler. Logs errors and appends issue template if error is unknown.
  */
-export const andHandleWith = (issueTemplateURL: string) => (error: Error) => {
+export const andHandle = (error: Error) => {
   const knownErrors = Object.keys(ERRORS);
 
   console.error(error);
 
   if (!knownErrors.includes(error.name)) {
-    console.log(`${ISSUE_TEMPLATE}${issueTemplateURL}`);
+    console.log(ISSUE_TEMPLATE);
   }
 };
