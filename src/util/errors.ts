6fd1714252c07a90a155528fd225184aef867f1e
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-12-09 10:54:33 +0400

    util: init input validaiton error.

--- src/util/errors.ts
+++ src/util/errors.ts
@@ -1,6 +1,7 @@
 const CUSTOM_ERRORS = [
   'CliInputError',
   'ImplValidationError',
+  'InputValidationError',
   'InvalidAggregationParams',
   'ModelInitializationError',
   'ModelCredentialError',
