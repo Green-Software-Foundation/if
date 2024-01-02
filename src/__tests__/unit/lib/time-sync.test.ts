d25d835d0786e7e7a4cfd445fc853b0342c24c46
jmc <33655003+jmcook1186@users.noreply.github.com>
2024-01-02 17:15:28 +0000

    add init and configure tests

--- src/__tests__/unit/lib/time-sync.test.ts
+++ src/__tests__/unit/lib/time-sync.test.ts
@@ -16,14 +16,22 @@ describe('models/time-sync: ', () => {
     describe('initialize time model', () => {
         it('initializes object with required properties.', () => {
             const timeModel = new TimeSyncModel();
-            expect(typeof timeModel.configure({
-                'start-time': '2023-12-12T00:00:00.000Z',
-                'end-time': '2023-12-12T00:01:00.000Z',
-                interval: 5
-            })).toBe('object');
-            expect(timeModel.startTime).toEqual('2023-12-12T00:00:00.000Z');
-            expect(timeModel.endTime).toEqual('2023-12-12T00:01:00.000Z');
-            expect(timeModel.interval).toEqual(5);
+            expect(timeModel).toHaveProperty('configure');
+            expect(timeModel).toHaveProperty('execute');
         });
+    });
+});
+
+describe('configure time model', () => {
+    it('configures object with required properties.', () => {
+        const timeModel = new TimeSyncModel();
+        expect(typeof timeModel.configure({
+            'start-time': '2023-12-12T00:00:00.000Z',
+            'end-time': '2023-12-12T00:01:00.000Z',
+            interval: 5
+        })).toBe('object');
+        expect(timeModel.startTime).toEqual('2023-12-12T00:00:00.000Z');
+        expect(timeModel.endTime).toEqual('2023-12-12T00:01:00.000Z');
+        expect(timeModel.interval).toEqual(5);
     })
 });
