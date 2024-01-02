cc1d27bfcc3d75cd893c83dfd475250a197fb691
jmc <33655003+jmcook1186@users.noreply.github.com>
2024-01-02 17:03:21 +0000

    add initial unit test for time sync

---
+++ src/__tests__/unit/lib/time-sync.test.ts
@@ -0,0 +1,29 @@
+import { TimeSyncModel } from '../../../models';
+
+// const {ModelInitializationError} = ERRORS;
+
+// const {
+//   MISSING_CLASSNAME,
+//   MISSING_PATH,
+//   NOT_INITIALIZED_MODEL,
+//   NOT_NATIVE_MODEL,
+//   NOT_MODEL_PLUGIN_EXTENSION,
+//   NOT_CONSTRUCTABLE_MODEL,
+//   INVALID_MODULE_PATH,
+// } = STRINGS;
+
+describe('models/time-sync: ', () => {
+    describe('initialize time model', () => {
+        it('initializes object with required properties.', () => {
+            const timeModel = new TimeSyncModel();
+            expect(typeof timeModel.configure({
+                'start-time': '2023-12-12T00:00:00.000Z',
+                'end-time': '2023-12-12T00:01:00.000Z',
+                interval: 5
+            })).toBe('object');
+            expect(timeModel.startTime).toEqual('2023-12-12T00:00:00.000Z');
+            expect(timeModel.endTime).toEqual('2023-12-12T00:01:00.000Z');
+            expect(timeModel.interval).toEqual(5);
+        });
+    })
+});
