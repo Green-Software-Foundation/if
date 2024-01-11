1ef582574c3a76068c2b580fe2d83e95e3ec6214
Narek Hovhannisyan <narekgilmour@gmail.com>
2024-01-11 20:12:11 +0400

    config: add aggregation errors to string.

--- src/config/strings.ts
+++ src/config/strings.ts
@@ -34,4 +34,8 @@ https://github.com/Green-Software-Foundation/if/issues/new?assignees=&labels=fee
   INVALID_TIME_NORMALIZATION: 'Start time or end time is missing.',
   INVALID_TIME_INTERVAL: 'Interval is missing.',
   INVALID_OBSERVATION_OVERLAP: 'Observation timestamps overlap.',
+  INVALID_AGGREGATION_METHOD: (metric: string) =>
+    `Aggregation is not possible for given ${metric} since method is 'none'.`,
+  METRIC_MISSING: (metric: string, index: number) =>
+    `Aggregation metric ${metric} is not found in inputs[${index}].`,
 };
