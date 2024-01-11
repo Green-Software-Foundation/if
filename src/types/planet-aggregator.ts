cafdeaad5de20c73cda22ae3c1d952583cd70696
Narek Hovhannisyan <narekgilmour@gmail.com>
2024-01-11 19:45:52 +0400

    types: drop unused aggregation fields.

--- src/types/planet-aggregator.ts
+++ src/types/planet-aggregator.ts
@@ -1,7 +1,1 @@
-import {AggregationMetrics} from './impl';
-
-export type PlanetAggregatorParams = {
-  'aggregation-metrics': AggregationMetrics[];
-};
-
 export type AggregationResult = Record<string, number>;
