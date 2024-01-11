0283dd58a035d92c2da9448f90f4d1f80d855bbf
Narek Hovhannisyan <narekgilmour@gmail.com>
2024-01-12 01:37:27 +0400

    types: add aggregation metods.

--- src/types/planet-aggregator.ts
+++ src/types/planet-aggregator.ts
@@ -1,1 +1,10 @@
 export type AggregationResult = Record<string, number>;
+
+export const AggregationMethods = [
+  'none',
+  'horizontal',
+  'vertical',
+  'both',
+] as const;
+
+export type AggregationMethodsName = (typeof AggregationMethods)[number];
