4b23f9aa32ae133f1942202687fab7e7f9a2d79d
Narek Hovhannisyan <narekgilmour@gmail.com>
2024-02-01 16:07:31 +0400

    types: tune units.

--- src/types/units.ts
+++ src/types/units.ts
@@ -2,23 +2,25 @@ import {PARAMETERS} from '../config';
 
 export type ParameterKey = keyof typeof PARAMETERS | string;
 
+export const AGGREGATION_TYPES = ['sum', 'none', 'avg'] as const;
+
+export type AggregationTypes = (typeof AGGREGATION_TYPES)[number];
+
 export type Units = {
   [key: string]: Parameter;
 };
 
-export type Parameters = {
-  [key: ParameterKey]: ParameterFields;
-};
+export type Parameters = typeof PARAMETERS;
 
 export type ParameterFields = {
   unit: string;
   description: string;
-  aggregation: 'sum' | 'none' | 'avg';
+  aggregation: AggregationTypes;
 };
 
 export type Parameter = {
   name: string;
   unit: string;
   description: string;
-  aggregation: 'sum' | 'none' | 'avg';
+  aggregation: AggregationTypes;
 };
