79e94d680445b524bb3edf176e7ed027d4449d68
Narek Hovhannisyan <narekgilmour@gmail.com>
2024-02-01 17:30:20 +0400

    types: in units drop parameters dependency.

--- src/types/units.ts
+++ src/types/units.ts
@@ -1,16 +1,10 @@
-import {PARAMETERS} from '../config';
-
-export type ParameterKey = keyof typeof PARAMETERS | string;
-
 export const AGGREGATION_TYPES = ['sum', 'none', 'avg'] as const;
 
 export type AggregationTypes = (typeof AGGREGATION_TYPES)[number];
 
 export type Units = {
-  [key: string]: Parameter;
+  [key: string]: ParameterFields;
 };
-
-export type Parameters = typeof PARAMETERS;
 
 export type ParameterFields = {
   unit: string;
