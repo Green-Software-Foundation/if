fbe7897a12c80745d5ffae862a2a1ef61a91ba0a
Narek Hovhannisyan <narekgilmour@gmail.com>
2024-02-12 16:01:55 +0400

    refactor(lib): implement load

---
+++ src/lib/load.ts
@@ -0,0 +1,27 @@
+import {validateManifest} from '../util/validations';
+import {openYamlFileAsObject} from '../util/yaml';
+
+import {Manifest} from '../types/manifest';
+import {ContextTree} from '../types/load';
+
+/**
+ * Parses YAML file as a object, then contructs tree, context interface.
+ */
+export const load = async (inputPath: string): Promise<ContextTree> => {
+  const safeManifest = await openYamlFileAsObject<Manifest>(inputPath);
+  const {initialize, aggregation, params, tags, description, name, tree} =
+    validateManifest(safeManifest); // TODO: should be moved outside as separate call
+
+  return {
+    safeManifest,
+    tree,
+    context: {
+      initialize,
+      aggregation,
+      params,
+      tags,
+      description,
+      name,
+    },
+  };
+};
