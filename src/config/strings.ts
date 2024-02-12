907d9217d2fd12b6748e9810a16643b8236f2c88
Narek Hovhannisyan <narekgilmour@gmail.com>
2024-02-12 15:32:23 +0400

    chore(config): in strings update messages to use plugin

--- src/config/strings.ts
+++ src/config/strings.ts
@@ -1,5 +1,3 @@
-import {ClassContainerParams} from '../types/models-universe';
-
 export const STRINGS = {
   FILE_IS_NOT_YAML: 'Provided impl file is not in yaml format.',
   IMPL_IS_MISSING: 'Impl file is missing.',
@@ -7,32 +5,30 @@ export const STRINGS = {
   MISSING_PATH: "Initalization param 'path' is missing.",
   OVERRIDE_WARNING:
     '\n**WARNING**: You are overriding the IF default parameters file. Please be extremely careful of unintended side-effects in your plugin pipeline!\n',
-  NOT_MODEL_PLUGIN_EXTENSION:
-    "Provided model does not extend 'ModelPluginInterface'.",
+  NOT_PLUGIN_PLUGIN_EXTENSION:
+    "Provided plugin does not extend 'PluginInterface'.",
   STRUCTURE_MALFORMED: (childName: string) =>
     `Graph is malformed: graph.children.${childName} is not valid.`,
-  NOT_INITIALIZED_MODEL: (className: string) =>
-    `You're trying to use not initalized model: ${className}.`,
+  NOT_INITIALIZED_PLUGIN: (className: string) =>
+    `You're trying to use not initalized plugin: ${className}.`,
   DISCLAIMER_MESSAGE: `
 [!important] Incubation Project
 
 This project is an incubation project being run inside the Green Software Foundation; as such, we *DON’T recommend using it in any critical use case. 
 Incubation projects are experimental, offer no support guarantee, have minimal governance and process, and may be retired at any moment. This project may one day graduate, in which case this disclaimer will be removed.
 `,
-  NOT_NATIVE_MODEL: `
+  NOT_NATIVE_PLUGIN: `
 [!important]
 
-You are using models that are not part of the Impact Framework standard library. You should do your own research to ensure the models are up to date and accurate. They may not be actively maintained.  
+You are using plugins that are not part of the Impact Framework standard library. You should do your own research to ensure the PLUGINs are up to date and accurate. They may not be actively maintained.  
 `,
   SOMETHING_WRONG: 'Something wrong with cli arguments. Please check docs.',
   ISSUE_TEMPLATE: `
 Impact Framework is an alpha release from the Green Software Foundation and is released to capture early feedback. If you'd like to offer some feedback, please use this issue template: 
 https://github.com/Green-Software-Foundation/if/issues/new?assignees=&labels=feedback&projects=&template=feedback.md&title=Feedback+-+
 `,
-  NOT_CONSTRUCTABLE_MODEL: (params: ClassContainerParams) =>
-    `Provided model '${params.model}' is not constructable or does not belong to given plugin '${params.path}'.`,
   INVALID_MODULE_PATH: (path: string) =>
-    `Provided module path: '${path}' is invalid.`,
+    `Provided module path: '${path}' is invalid or not found.`,
   INVALID_TIME_NORMALIZATION: 'Start time or end time is missing.',
   INVALID_TIME_INTERVAL: 'Interval is missing.',
   AVOIDING_PADDING: (description: string) =>
