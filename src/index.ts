ae3e0715d736f0369a7c555d86cd363886497e80
jmc <33655003+jmcook1186@users.noreply.github.com>
2024-02-13 10:26:37 +0000

    feat: [aggregate] invoke aggregation in index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import {ERRORS} from './util/errors';
 import {andHandle} from './util/helpers';
 import {logger} from './util/logger';
 import {saveYamlFileAs} from './util/yaml';
-
+import {aggregate} from './lib/aggregator';
 import {STRINGS} from './config';
 
 import {initalize} from './lib/initialize';
@@ -21,14 +21,13 @@ const impactEngine = async () => {
 
   if (options) {
     const {inputPath, outputPath} = options;
-
     const {tree, context, safeManifest} = await load(inputPath);
     const plugins = await initalize(context.initialize.plugins);
     const computedTree = await compute(tree, context, plugins);
-
+    const aggregatedTree = aggregate(computedTree, context);
     const outputFile = {
       ...safeManifest,
-      tree: computedTree,
+      tree: aggregatedTree,
     };
 
     if (!outputPath) {
