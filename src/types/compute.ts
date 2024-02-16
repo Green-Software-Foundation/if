0ccb8f92b7659114995e80e74b5d09ce15e6bd24
Narek Hovhannisyan <narekgilmour@gmail.com>
2024-02-16 16:37:56 +0400

    chore(types): separate NodeCOnfig in compute

--- src/types/compute.ts
+++ src/types/compute.ts
@@ -2,18 +2,22 @@ import {PluginsStorage} from './initialize';
 import {PluginParams} from './interface';
 import {ManifestCommon} from './manifest';
 
+export type NodeConfig = {
+  [key: string]: Record<string, any>;
+};
+
 export type Params = {
   plugins: PluginsStorage;
   context: ManifestCommon;
   pipeline?: string[];
-  config?: Record<string, any>;
+  config?: NodeConfig;
   defaults?: PluginParams[];
 };
 
 export type Node = {
   children?: any;
   pipeline?: string[];
-  config?: Record<string, any>;
+  config?: NodeConfig;
   defaults?: PluginParams[];
   inputs?: PluginParams[];
   outputs?: PluginParams[];
