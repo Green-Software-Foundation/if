d69314678c392d70a769cd20d024efd75bee78b9
MariamKhalatova <mar.khalatova@gmail.com>
2024-02-24 01:40:55 +0400

    style: testing DCO setup

--- src/__tests__/integration/scenarios/sci-e.ts
+++ src/__tests__/integration/scenarios/sci-e.ts
@@ -60,6 +60,7 @@ describe('integration/sci-e', () => {
     expect(path.energy).toEqual(sum);
   });
 
+  //after
   afterAll(() => {
     return npmUninstallPackage('@grnsft/if-models');
   }, 15000);
