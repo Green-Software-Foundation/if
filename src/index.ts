2c8720a4a94df44ec26b54e8e48cae4d0f5f2917
Narek Hovhannisyan <narekgilmour@gmail.com>
2024-02-27 22:33:42 +0400

    feat(src): implement cli/manifest parameters logic

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
+import {aggregate} from './lib/aggregate';
+import {compute} from './lib/compute';
+import {initalize} from './lib/initialize';
+import {load} from './lib/load';
+import {parameterize} from './lib/parameterize';
+
 import {parseArgs} from './util/args';
 import {ERRORS} from './util/errors';
 import {andHandle} from './util/helpers';
@@ -6,11 +12,6 @@ import {logger} from './util/logger';
 import {saveYamlFileAs} from './util/yaml';
 
 import {STRINGS} from './config';
-
-import {initalize} from './lib/initialize';
-import {compute} from './lib/compute';
-import {load} from './lib/load';
-import {aggregate} from './lib/aggregate';
 
 const {CliInputError} = ERRORS;
 
@@ -21,11 +22,12 @@ const impactEngine = async () => {
   const options = parseArgs();
 
   if (options) {
-    const {inputPath, outputPath} = options;
+    const {inputPath, outputPath, paramPath} = options;
 
-    const {tree, context} = await load(inputPath);
+    const {tree, context, parameters} = await load(inputPath, paramPath);
+    parameterize.combine(context.params, parameters);
     const plugins = await initalize(context.initialize.plugins);
-    const computedTree = await compute(tree, context, plugins);
+    const computedTree = await compute(tree, {context, plugins});
     const aggregatedTree = aggregate(computedTree, context.aggregation);
 
     const outputFile = {
