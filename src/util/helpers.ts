513fb9cfb0a279ed08546bccc556abe54d94db81
Narek Hovhannisyan <narekgilmour@gmail.com>
2024-02-28 14:54:44 +0400

    feat(util): implement merge objects helper

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -17,3 +17,24 @@ export const andHandle = (error: Error) => {
     logger.warn(ISSUE_TEMPLATE);
   }
 };
+
+/**
+ * Mergers two objects, omitting null values.
+ */
+export const mergeObjects = (object1: any, object2: any) => {
+  const merged: Record<string, any> = {};
+
+  const keys1 = Object.keys(object1);
+  keys1.forEach(key1 => {
+    merged[key1] = object2[key1] || object1[key1];
+  });
+
+  const keys2 = Object.keys(object2);
+  keys2.forEach(key2 => {
+    if (!keys1.includes(key2)) {
+      merged[key2] = object2[key2];
+    }
+  });
+
+  return merged;
+};
