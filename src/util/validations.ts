48863b49a3bd53c9787bda812e2d925361e94ba8
Narek Hovhannisyan <narekgilmour@gmail.com>
2024-03-02 01:46:28 +0400

    feat(util): make outputs array in validations

--- src/util/validations.ts
+++ src/util/validations.ts
@@ -49,7 +49,7 @@ const manifestValidation = z.object({
         'global-config': z.record(z.string(), z.any()).optional(),
       })
     ),
-    outputs: z.record(z.string(), z.record(z.string(), z.any()).optional()),
+    outputs: z.array(z.string()),
   }),
   tree: z.record(z.string(), z.any()),
 });
