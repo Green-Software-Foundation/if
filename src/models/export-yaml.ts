10b5e5f2afd2ddc2396d9e348337dc946910e917
Narek Hovhannisyan <narekgilmour@gmail.com>
2024-03-02 01:50:37 +0400

    feat(models): implement export yaml

---
+++ src/models/export-yaml.ts
@@ -0,0 +1,27 @@
+import {saveYamlFileAs} from '../util/yaml';
+
+import {ERRORS} from '../util/errors';
+
+import {Context} from '../types/manifest';
+
+const {CliInputError} = ERRORS;
+
+export const ExportYaml = () => {
+  /**
+   * Saves output file in YAML format.
+   */
+  const execute = async (tree: any, context: Context, outputPath: string) => {
+    if (!outputPath) {
+      throw new CliInputError('Output path is required.');
+    }
+
+    const outputFile = {
+      ...context,
+      tree,
+    };
+
+    await saveYamlFileAs(outputFile, outputPath);
+  };
+
+  return {execute};
+};
