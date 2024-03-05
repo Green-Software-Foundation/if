f8baf036cf49f03806ab4384af1fba122b5bd8ce
Narek Hovhannisyan <narekgilmour@gmail.com>
2024-03-06 00:35:13 +0400

    refactor(types): tune parameters to use zod infered

--- src/types/parameters.ts
+++ src/types/parameters.ts
@@ -1,18 +1,7 @@
+import {ManifestParameter} from './manifest';
+
 export const AGGREGATION_TYPES = ['sum', 'none', 'avg'] as const;
 
-type AggregationTypes = (typeof AGGREGATION_TYPES)[number];
-
-type ParameterProps = {
-  unit: string;
-  description: string;
-  aggregation: AggregationTypes;
-};
-
-export type ManifestParameter = {
-  name: string;
-  unit: string;
-  description: string;
-  aggregation: AggregationTypes;
-};
+type ParameterProps = Omit<ManifestParameter, 'name'>;
 
 export type Parameters = Record<string, ParameterProps>;
