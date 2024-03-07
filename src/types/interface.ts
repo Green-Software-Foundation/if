b8857cb1d3020aa998000c85ffd10e8ad6ae0053
Narek Hovhannisyan <narekgilmour@gmail.com>
2024-03-07 10:05:40 +0400

    feat(types): implement advanced interface

--- src/types/interface.ts
+++ src/types/interface.ts
@@ -1,12 +1,19 @@
+import {Context} from './manifest';
+
 export type PluginParams = Record<string, any>;
 
-export type PluginInterface = {
-  execute: (
-    inputs: PluginParams[],
-    config?: Record<string, any>
-  ) => PluginParams[];
+type PluginType = 'execute' | 'groupby' | 'exhaust';
+
+export interface PluginInterface<T extends PluginType> {
+  execute: T extends 'execute'
+    ? (inputs: PluginParams[], config?: Record<string, any>) => PluginParams[]
+    : T extends 'groupby'
+    ? (inputs: PluginParams[], config: {group: string[]}) => {children: any}
+    : T extends 'exhaust'
+    ? (tree: any, context: Context, outputPath?: string) => Promise<void>
+    : never;
   metadata: {
-    kind: string;
+    kind: T;
   };
   [key: string]: any;
-};
+}
