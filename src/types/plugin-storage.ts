27f043bf9ae99f0fd81054f680c01bb3849e6cb3
Paz Barda <paz.barda@intel.com>
2024-04-01 10:58:20 +0300

    feat(src): created storage entities for exhaust plugins
    Signed-off-by: Paz Barda <paz.barda@intel.com>

--- src/types/plugin-storage.ts
+++ src/types/plugin-storage.ts
@@ -1,8 +1,16 @@
-import {pluginStorage} from '../util/plugin-storage';
-import {PluginInterface} from './interface';
+import {exhaustPluginStorage, pluginStorage} from '../util/plugin-storage';
+import {ExhaustPluginInterface, PluginInterface} from './interface';
 
 export type PluginStorage = {
   [key: string]: PluginInterface;
 };
 
 export type PluginStorageInterface = ReturnType<typeof pluginStorage>;
+
+export type ExhaustPluginsStorage = {
+  [key: string]: ExhaustPluginInterface;
+};
+
+export type ExhaustPluginStorageInterface = ReturnType<
+  typeof exhaustPluginStorage
+>;
--- src/util/plugin-storage.ts
+++ src/util/plugin-storage.ts
@@ -1,8 +1,8 @@
 import {ERRORS} from '../util/errors';
 import {STRINGS} from '../config';
 
-import {PluginInterface} from '../types/interface';
-import {PluginStorage} from '../types/plugin-storage';
+import {ExhaustPluginInterface, PluginInterface} from '../types/interface';
+import {ExhaustPluginsStorage, PluginStorage} from '../types/plugin-storage';
 
 const {PluginInitalizationError} = ERRORS;
 const {NOT_INITALIZED_PLUGIN} = STRINGS;
@@ -30,6 +30,36 @@ export const pluginStorage = () => {
      * Saves given `plugin` with given `name`.
      */
     set: function (name: string, plugin: PluginInterface) {
+      storage[name] = plugin;
+
+      return this;
+    },
+  };
+};
+
+/**
+ * Storage for maintaining exhaust plugins.
+ */
+export const exhaustPluginStorage = () => {
+  const storage: ExhaustPluginsStorage = {};
+
+  return {
+    /**
+     * Gets plugin by given `name`. If it's missing then throws error.
+     */
+    get: (name: string) => {
+      const plugin = storage[name];
+
+      if (!plugin) {
+        throw new PluginInitalizationError(NOT_INITALIZED_PLUGIN(name));
+      }
+
+      return plugin;
+    },
+    /**
+     * Saves given `plugin` with given `name`.
+     */
+    set: function (name: string, plugin: ExhaustPluginInterface) {
       storage[name] = plugin;
 
       return this;
