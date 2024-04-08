718d8412f8f85f15f9943c1bd3a44ef2ed2175f6
Narek Hovhannisyan <narekgilmour@gmail.com>
2024-04-08 22:19:17 +0400

    feat(util): add warning if output and stdout are missing

--- src/util/args.ts
+++ src/util/args.ts
@@ -3,6 +3,7 @@ import {parse} from 'ts-command-line-args';
 
 import {checkIfFileIsYaml} from './yaml';
 import {ERRORS} from './errors';
+import {logger} from './logger';
 
 import {CONFIG, STRINGS} from '../config';
 
@@ -11,7 +12,7 @@ import {ManifestProcessArgs} from '../types/process-args';
 const {CliInputError} = ERRORS;
 
 const {impact} = CONFIG;
-const {ARGS, HELP} = impact;
+const {ARGS, HELP, NO_OUTPUT} = impact;
 
 const {FILE_IS_NOT_YAML, MANIFEST_IS_MISSING} = STRINGS;
 
@@ -46,6 +47,7 @@ const prependFullFilePath = (filePath: string) => {
 /**
  * 1. Parses process arguments like `manifest`, `output`, `override-params` and `help`.
  * 2. Checks if `help` param is provided, then logs help message and exits.
+ * 3. If output params are missing, warns user about it.
  * 3. Otherwise checks if `manifest` param is there, then processes with checking if it's a yaml file.
  *    If it is, then returns object containing full path.
  * 4. If params are missing or invalid, then rejects with `CliInputError`.
@@ -64,13 +66,17 @@ export const parseArgs = () => {
     return;
   }
 
+  if (!output && !stdout) {
+    logger.warn(NO_OUTPUT);
+  }
+
   if (manifest) {
     if (checkIfFileIsYaml(manifest)) {
       return {
         inputPath: prependFullFilePath(manifest),
         outputOptions: {
           ...(output && {outputPath: prependFullFilePath(output)}),
-          stdout,
+          ...(stdout && {stdout}),
         },
         ...(overrideParams && {paramPath: overrideParams}),
       };
