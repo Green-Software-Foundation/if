28a41a184828a974b2c7847d2316c9e39d04ad01
Narek Hovhannisyan <narekgilmour@gmail.com>
2024-04-10 15:42:01 +0400

    feat(util): setup winston for upgraded logging

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,22 +1,29 @@
 import * as winston from 'winston';
 
-const {combine, timestamp, printf, colorize, align} = winston.format;
+const {combine, printf, colorize} = winston.format;
+
+type LoggerScopes = 'CLI' | 'Manifest' | 'Plugin';
+
+/**
+ * Setups the pattern for logging.
+ */
+const alignAllColors = (meta: LoggerScopes) =>
+  combine(
+    printf((info: any) => {
+      const namePattern = info.name ? `${info.name}: ` : '';
+      const stackPattern = info.stack ?? '';
+      const messagePattern = `${info.message}${info.stack ? '\n' : ''}`;
+
+      return `(${info.level})[${meta}] ${namePattern}${messagePattern}${stackPattern}\n`;
+    }),
+    colorize({all: true})
+  );
 
 /**
  * Winston logger instance.
  */
-export const logger = winston.createLogger({
-  format: combine(
-    colorize({all: true}),
-    timestamp({
-      format: 'YYYY-MM-DD hh:mm:ss.SSS A',
-    }),
-    align(),
-    printf(
-      (info: any) =>
-        `[${info.timestamp}] ${info.level}: ${info.message}
-${info.stack || ''}`
-    )
-  ),
-  transports: [new winston.transports.Console()],
-});
+export const Logger = (meta: LoggerScopes) =>
+  winston.createLogger({
+    format: combine(alignAllColors(meta)),
+    transports: [new winston.transports.Console()],
+  });
