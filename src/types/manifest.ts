638451b4f5733aa449cdcd2cbfee8fac77bbf5ac
Narek Hovhannisyan <narhovhannisian@gmail.com>
2024-05-08 17:51:29 +0400

    fix(types): remove manually declared types

--- src/types/manifest.ts
+++ src/types/manifest.ts
@@ -12,20 +12,7 @@ export type AggregationParams = Manifest['aggregation'];
 export type AggregationParamsSure = Extract<Manifest['aggregation'], {}>;
 
 export type Context = Omit<Manifest, 'tree'>;
-export type ContextWithExec = Omit<Manifest, 'tree'> & {
-  execution: {
-    command: string;
-    environment: {
-      'if-version': string;
-      os: string;
-      'os-version': string;
-      'node-version': string;
-      'date-time': string;
-      dependencies: string[];
-    };
-    status: 'success' | 'fail';
-    error?: string;
-  };
-};
+
+export type ContextWithExec = Omit<Manifest, 'tree'>;
 
 export type ManifestParameter = Extract<Manifest['params'], {}>[number];
