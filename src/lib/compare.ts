8c2546bb68aba81fa104415253c1db6001107a3a
Narek Hovhannisyan <narhovhannisian@gmail.com>
2024-05-15 18:54:09 +0400

    feat(lib): implement compare

---
+++ src/lib/compare.ts
@@ -0,0 +1,52 @@
+import {oneIsPrimitive} from '../util/helpers';
+
+import {Difference} from '../types/lib/compare';
+
+/**
+ * 1. If objects are not of the same type or are primitive types, compares directly.
+ * 2. Gets the keys from both objects.
+ * 3. Checks for keys present in both objects.
+ * 4. If both are arrays, checks their elements.
+ * 5. Checks for differences in values for common keys.
+ * 6. If all common keys are checked and no differences are found, return empty object.
+ */
+export const compare = (source: any, target: any, path = ''): Difference => {
+  if (oneIsPrimitive(source, target)) {
+    return source !== target
+      ? {
+          path,
+          source: source,
+          target: target,
+        }
+      : {};
+  }
+
+  const keys1 = Object.keys(source);
+  const keys2 = Object.keys(target);
+
+  const allKeys = new Set([...keys1, ...keys2]);
+
+  if (Array.isArray(source) && Array.isArray(target)) {
+    if (source.length !== target.length) {
+      return {path, source: source, target: target};
+    }
+
+    for (let i = 0; i < source.length; i++) {
+      return compare(source[i], target[i], path ? `${path}[${i}]` : `${i}`);
+    }
+  }
+
+  for (const key of allKeys) {
+    const result = compare(
+      source[key],
+      target[key],
+      path ? `${path}.${key}` : key
+    );
+
+    if (Object.keys(result).length) {
+      return result;
+    }
+  }
+
+  return {};
+};
