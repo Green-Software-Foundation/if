ebb348c2ff6d571ace2793bdcee065e7995a2a48
Narek Hovhannisyan <narhovhannisian@gmail.com>
2024-05-15 18:35:22 +0400

    feat(util): add if-diff, readline helper

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -1,3 +1,4 @@
+import {createInterface} from 'node:readline/promises';
 import {exec} from 'node:child_process';
 import {promisify} from 'node:util';
 
@@ -5,6 +6,8 @@ import {ERRORS} from './errors';
 import {logger} from './logger';
 
 import {STRINGS} from '../config';
+
+import {Difference} from '../types/lib/compare';
 
 const {ISSUE_TEMPLATE} = STRINGS;
 
@@ -44,3 +47,110 @@ export const mergeObjects = (defaults: any, input: any) => {
  * Promise version of Node's `exec` from `child-process`.
  */
 export const execPromise = promisify(exec);
+
+/**
+ * Converts given `value` to either `1` or `0`.
+ */
+const convertToXorable = (value: any) => {
+  if (typeof value === 'number') {
+    return value !== 0 ? 1 : 0;
+  }
+
+  if (typeof value === 'boolean') {
+    return value ? 1 : 0;
+  }
+
+  if (typeof value === 'string') {
+    return value.length > 0 ? 1 : 0;
+  }
+
+  return 0;
+};
+
+/**
+ * If one of the `valuesToCheck` values is undefined, then set `missing`, otherwise `exists`.
+ */
+const setValuesIfMissing = (response: Difference) => {
+  const source = convertToXorable(response.source);
+  const target = convertToXorable(response.target);
+
+  if (source ^ target) {
+    ['source', 'target'].forEach(value => {
+      response[value] = response[value] ? 'exists' : 'missing';
+    });
+
+    return response;
+  }
+
+  return response;
+};
+
+/**
+ * Checks if objects are primitive types.
+ */
+export const oneIsPrimitive = (source: any, target: any) => {
+  // eslint-disable-next-line eqeqeq
+  if (source == null || target == null) {
+    return true;
+  }
+
+  return source !== Object(source) && target !== Object(target);
+};
+
+/**
+ * Format not matching message for CLI logging.
+ */
+export const formatNotMatchingLog = (message: Difference) => {
+  const flattenMessage = setValuesIfMissing(message);
+
+  Object.keys(flattenMessage).forEach(key => {
+    if (key === 'message' || key === 'path') {
+      console.log(message[key]);
+    } else {
+      console.log(`${key}: ${message[key]}`);
+    }
+  });
+};
+
+/**
+ * Checks if there is data piped, then collects it.
+ * Otherwise returns empty string.
+ */
+const collectPipedData = async () => {
+  if (process.stdin.isTTY) {
+    return;
+  }
+
+  const readline = createInterface({
+    input: process.stdin,
+  });
+
+  const data: string[] = [];
+
+  for await (const line of readline) {
+    data.push(line);
+  }
+
+  return data.join('\n');
+};
+
+/**
+ * Checks if there is piped data, tries to parse yaml from it.
+ * Throws error if there is piped info, but there is no valid manifest.
+ */
+export const parseManifestFromStdin = async () => {
+  const pipedSourceManifest = await collectPipedData();
+
+  if (!pipedSourceManifest) {
+    return '';
+  }
+
+  const regex = /# start((?:.*\n)+?)# end/;
+  const match = regex.exec(pipedSourceManifest);
+
+  if (!match) {
+    throw new Error('Manifest not found in STDIN.');
+  }
+
+  return match![1];
+};
