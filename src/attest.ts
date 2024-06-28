0d8e0b7ef9465347ac74e5c522f54b91ae7b6585
jmc <33655003+jmcook1186@users.noreply.github.com>
2024-06-28 17:00:09 +0100

    feat(lib): add if-check step to attest

--- src/attest.ts
+++ src/attest.ts
@@ -5,22 +5,30 @@ import {readFileSync} from 'fs';
 import * as YAML from 'js-yaml';
 const packageJson = require('../package.json');
 import {SchemaEncoder} from '@ethereum-attestation-service/eas-sdk';
+import {execPromise} from './util/helpers';
 
 const IfAttest = async (manifestPath: string) => {
   const manifestHash = GetManifestHash(manifestPath);
   const ifVersion = GetIfVersion();
-  const encodedSchema = encodeSchema(manifestHash, ifVersion);
-  console.log(manifestHash, ifVersion);
+  const isVerified = await runIfCheck(manifestPath);
+  const encodedSchema = encodeSchema(manifestHash, ifVersion, isVerified);
+
+  console.log(manifestHash, ifVersion, isVerified);
   console.log(encodedSchema);
 };
 
-const encodeSchema = (manifestHash: string, ifVersion: string) => {
+const encodeSchema = (
+  manifestHash: string,
+  ifVersion: string,
+  isVerified: boolean
+) => {
   const schemaEncoder = new SchemaEncoder(
-    'bytes32 manifestHash, string ifVersion'
+    'bytes32 manifestHash, string ifVersion, bool isVerified'
   );
   const encodedData = schemaEncoder.encodeData([
     {name: 'manifestHash', value: manifestHash, type: 'bytes32'},
     {name: 'ifVersion', value: ifVersion, type: 'string'},
+    {name: 'isVerified', value: isVerified, type: 'bool'},
   ]);
   return encodedData;
 };
@@ -37,4 +45,18 @@ const GetIfVersion = (): string => {
   return packageJson.version;
 };
 
-IfAttest('/home/joe/Code/if/manifests/examples/sci.yml');
+const runIfCheck = async (manifestPath: string): Promise<boolean> => {
+  const response = await execPromise(`npm run if-check -- -m ${manifestPath}`, {
+    cwd: process.env.CURRENT_DIR || process.cwd(),
+  });
+
+  if (response.stdout.includes('if-check could not verify')) {
+    console.log('IF-CHECK: verification was unsuccessful. Files do not match');
+    return false;
+  }
+  console.log('IF-CHECK: verification was successful');
+
+  return true;
+};
+
+IfAttest('/home/joe/Code/if/manifests/outputs/teads.yaml');
