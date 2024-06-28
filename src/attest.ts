28b6bab7afc3f5b768a523a8c44706280f9f3d49
jmc <33655003+jmcook1186@users.noreply.github.com>
2024-06-28 15:08:12 +0100

    feat(lib): encode schema data

--- src/attest.ts
+++ src/attest.ts
@@ -4,12 +4,25 @@ import {ethers} from 'ethers';
 import {readFileSync} from 'fs';
 import * as YAML from 'js-yaml';
 const packageJson = require('../package.json');
-//import { EAS } from '@ethereum-attestation-service/eas-sdk';
+import {SchemaEncoder} from '@ethereum-attestation-service/eas-sdk';
 
 const IfAttest = async (manifestPath: string) => {
   const manifestHash = GetManifestHash(manifestPath);
   const ifVersion = GetIfVersion();
+  const encodedSchema = encodeSchema(manifestHash, ifVersion);
   console.log(manifestHash, ifVersion);
+  console.log(encodedSchema);
+};
+
+const encodeSchema = (manifestHash: string, ifVersion: string) => {
+  const schemaEncoder = new SchemaEncoder(
+    'bytes32 manifestHash, string ifVersion'
+  );
+  const encodedData = schemaEncoder.encodeData([
+    {name: 'manifestHash', value: manifestHash, type: 'bytes32'},
+    {name: 'ifVersion', value: ifVersion, type: 'string'},
+  ]);
+  return encodedData;
 };
 
 const GetManifestHash = (manifestPath: string): string => {
--- src/util/register-eas-schema.ts
+++ src/util/register-eas-schema.ts
@@ -1,31 +1,33 @@
-import { SchemaRegistry } from '@ethereum-attestation-service/eas-sdk';
-import { ethers } from 'ethers';
+import {SchemaRegistry} from '@ethereum-attestation-service/eas-sdk';
+import {ethers} from 'ethers';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 const RPC_URL: string = process.env.RPC_URL ?? '';
-const REGISTRY_CONTRACT_ADDRESS: string = process.env.REGISTRY_CONTRACT_ADDRESS ?? '';
-const RESOLVER_CONTRACT_ADDRESS: string = process.env.RESOLVER_CONTRACT_ADDRESS ?? '';
+const REGISTRY_CONTRACT_ADDRESS: string =
+  process.env.REGISTRY_CONTRACT_ADDRESS ?? '';
+const RESOLVER_CONTRACT_ADDRESS: string =
+  process.env.RESOLVER_CONTRACT_ADDRESS ?? '';
 const PRIVATE_KEY: string = process.env.ETH_PRIVATE_KEY ?? '';
 const SCHEMA =
-    'uint256 manifestStart, uint256 manifestEnd, bytes32 manifestHash, uint256 carbon, uint256 sci, uint256 energy';
+  'uint256 manifestStart, uint256 manifestEnd, bytes32 manifestHash, uint256 carbon, uint256 sci, uint256 energy';
 
 const registerSchema = async () => {
-    const schemaRegistry = new SchemaRegistry(REGISTRY_CONTRACT_ADDRESS);
-    const provider = new ethers.JsonRpcProvider(RPC_URL);
-    const signer = new ethers.Wallet(PRIVATE_KEY, provider);
+  const schemaRegistry = new SchemaRegistry(REGISTRY_CONTRACT_ADDRESS);
+  const provider = new ethers.JsonRpcProvider(RPC_URL);
+  const signer = new ethers.Wallet(PRIVATE_KEY, provider);
 
-    schemaRegistry.connect(signer);
+  schemaRegistry.connect(signer);
 
-    const transaction = await schemaRegistry.register({
-        schema: SCHEMA,
-        resolverAddress: RESOLVER_CONTRACT_ADDRESS,
-        revocable: true,
-    });
+  const transaction = await schemaRegistry.register({
+    schema: SCHEMA,
+    resolverAddress: RESOLVER_CONTRACT_ADDRESS,
+    revocable: true,
+  });
 
-    // Optional: Wait for transaction to be validated
-    await transaction.wait();
+  // Optional: Wait for transaction to be validated
+  await transaction.wait();
 };
 
 registerSchema();
