d5365ead01e3bd8519d3227a5684eb7ad2e52301
jmc <33655003+jmcook1186@users.noreply.github.com>
2024-07-02 21:23:26 +0100

    feat(lib): make manifestInfo type and pass additional fields

--- src/attest.ts
+++ src/attest.ts
@@ -6,31 +6,78 @@ import * as YAML from 'js-yaml';
 const packageJson = require('../package.json');
 import {SchemaEncoder} from '@ethereum-attestation-service/eas-sdk';
 import {execPromise} from './util/helpers';
+import {openYamlFileAsObject} from './util/yaml';
+import {Manifest} from './types/manifest';
 
 const IfAttest = async (manifestPath: string) => {
-  const manifestHash = GetManifestHash(manifestPath);
-  const ifVersion = GetIfVersion();
-  const isVerified = await runIfCheck(manifestPath);
-  const encodedSchema = encodeSchema(manifestHash, ifVersion, isVerified);
+  //todo: make level a CLI argument
+  const level = 0;
 
-  console.log(manifestHash, ifVersion, isVerified);
+  const manifestInfo = await getManifestInfo(manifestPath, level);
+  const encodedSchema = encodeSchema(manifestInfo);
+
+  console.log(manifestInfo);
   console.log(encodedSchema);
 };
 
-const encodeSchema = (
-  manifestHash: string,
-  ifVersion: string,
-  isVerified: boolean
-) => {
+const encodeSchema = (manifestInfo: ManifestInfo) => {
   const schemaEncoder = new SchemaEncoder(
-    'bytes32 manifestHash, string ifVersion, bool isVerified'
+    'string start, string end, bytes32 hash, string if, bool verified, uint8 sci, string unit, uint8 energy, uint8 carbon, uint8 level'
   );
   const encodedData = schemaEncoder.encodeData([
-    {name: 'manifestHash', value: manifestHash, type: 'bytes32'},
-    {name: 'ifVersion', value: ifVersion, type: 'string'},
-    {name: 'isVerified', value: isVerified, type: 'bool'},
+    {name: 'start', value: manifestInfo.start, type: 'string'},
+    {name: 'end', value: manifestInfo.end, type: 'string'},
+    {name: 'hash', value: manifestInfo.hash, type: 'bytes32'},
+    {name: 'if', value: manifestInfo.if, type: 'string'},
+    {name: 'verified', value: manifestInfo.verified, type: 'bool'},
+    {name: 'sci', value: manifestInfo.sci, type: 'uint8'},
+    {name: 'unit', value: manifestInfo.unit, type: 'string'},
+    {name: 'energy', value: manifestInfo.energy, type: 'uint8'},
+    {name: 'carbon', value: manifestInfo.carbon, type: 'uint8'},
+    {name: 'level', value: manifestInfo.level, type: 'uint8'},
   ]);
   return encodedData;
+};
+
+type ManifestInfo = {
+  start: string;
+  end: string;
+  hash: string;
+  if: string;
+  verified: boolean;
+  sci: number;
+  unit: string; // aggregated SCI (from the top level of the `tree`)aggregate
+  energy: number; // aggregated energy (value from the top level of the `tree`)
+  carbon: number; // aggregated carbon (value from the top level of the `tree`)
+  level: number; // 0-5 GSF review thoroughness score
+};
+
+const getManifestInfo = async (
+  manifestPath: string,
+  level: number
+): Promise<ManifestInfo> => {
+  const file = await openYamlFileAsObject<Manifest>(manifestPath);
+
+  // const functionalUnitStub = file.initialize.plugins.sci['global-config']['functional-unit'] ??  '';
+  const functionalUnitStub = 'request'; //todo: DO NOT HARDCODE!
+  const unit = 'carbon per ' + functionalUnitStub;
+
+  const info: ManifestInfo = {
+    start: file.tree.children.child.outputs[0].timestamp,
+    end: file.tree.children.child.outputs[
+      file.tree.children.child.outputs.length - 1
+    ].timestamp,
+    hash: GetManifestHash(manifestPath),
+    if: GetIfVersion(),
+    verified: await runIfCheck(manifestPath),
+    sci: file.tree.aggregated.sci,
+    unit: unit,
+    energy: file.tree.aggregated.energy,
+    carbon: file.tree.aggregated.carbon,
+    level: level,
+  };
+
+  return info;
 };
 
 const GetManifestHash = (manifestPath: string): string => {
@@ -59,4 +106,4 @@ const runIfCheck = async (manifestPath: string): Promise<boolean> => {
   return true;
 };
 
-IfAttest('/home/joe/Code/if/manifests/outputs/teads.yaml');
+IfAttest('/home/joe/Code/if/manifests/outputs/coefficient.yaml');
