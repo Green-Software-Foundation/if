73e01a84cd6f05ab1a6f9e2e1c6e7ea9c71e72a1
Narek Hovhannisyan <narhovhannisian@gmail.com>
2024-07-05 23:43:29 +0400

    fix(config): add success message to if diff

--- src/if-diff/config/strings.ts
+++ src/if-diff/config/strings.ts
@@ -2,4 +2,5 @@ export const STRINGS = {
   INVALID_TARGET: 'Target is invalid.',
   INVALID_SOURCE: 'Source is invalid.',
   FAILURE_MESSAGE: 'Files do not match!',
+  SUCCESS_MESSAGE: 'Files match!',
 };
