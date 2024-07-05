3ed40e18ed9347953e8800f21ed50f10ebdbc5bc
Narek Hovhannisyan <narhovhannisian@gmail.com>
2024-07-05 14:20:32 +0400

    test(builtins): init if-run tests

---
+++ src/__tests__/if-run/builtins/export-csv-raw.test.ts
@@ -0,0 +1,77 @@
+import * as fs from 'fs/promises';
+
+import {jest} from '@jest/globals';
+import {ERRORS} from '@grnsft/if-core/utils';
+
+import {ExportCSVRaw} from '../../../if-run/builtins/export-csv-raw';
+
+import {STRINGS} from '../../../if-run/config';
+
+import {tree, context, outputs} from '../../../__mocks__/builtins/export-csv';
+
+const {ExhaustOutputArgError} = ERRORS;
+const {WRITE_CSV_ERROR, OUTPUT_REQUIRED} = STRINGS;
+
+jest.mock('fs/promises', () => ({
+  __esModule: true,
+  writeFile: jest.fn<() => Promise<void>>().mockResolvedValue(),
+}));
+
+describe('builtins/export-csv-raw: ', () => {
+  describe('ExportCSVRaw: ', () => {
+    const exportCSVRaw = ExportCSVRaw();
+
+    beforeEach(() => {
+      jest.resetAllMocks();
+    });
+
+    describe('init GroupBy: ', () => {
+      it('initalizes object with properties.', async () => {
+        expect(exportCSVRaw).toHaveProperty('execute');
+      });
+    });
+
+    describe('execute(): ', () => {
+      it('generates CSV file with correct data.', async () => {
+        const outputPath = 'output#carbon';
+        const content =
+          "id,timestamp,cloud/instance-type,region,duration,cpu/utilization,network/energy,energy,cpu/thermal-design-power,grid/carbon-intensity,device/emissions-embodied,time-reserved,device/expected-lifespan,resources-reserved,resources-total,cpu/energy,carbon-plus-energy',carbon-embodied,carbon-operational,carbon,sci\nchildren.child-1.outputs.0,2023-12-12T00:00:00.000Z,A1,uk-west,1,10,10,5,100,800,1533.12,3600,94608000,1,8,0.000008888888888888888,10.000008888888889,0.0000020256215119228817,4000,4000.0000020256216,240000.0001215373\nchildren.child-2.outputs.0,2023-12-12T00:00:00.000Z,A1,uk-west,1,30,10,5,100,800,1533.12,3600,94608000,1,8,0.00001650338753387534,10.000016503387533,0.0000020256215119228817,4000,4000.0000020256216,240000.0001215373\noutputs.0,2023-12-12T00:00:00.000Z,,,1,,,,,,,,,,,,,,,8000.000004051243,";
+
+        await exportCSVRaw.execute(tree, context, outputPath);
+
+        expect(fs.writeFile).toHaveBeenCalledWith(`${outputPath}.csv`, content);
+      });
+
+      it('throws an error when the CSV file could not be created.', async () => {
+        const outputPath = 'output#carbon';
+        const expectedMessage = 'Could not write CSV file.';
+
+        expect.assertions(1);
+
+        jest.spyOn(fs, 'writeFile').mockRejectedValue(expectedMessage);
+
+        await expect(
+          exportCSVRaw.execute(tree, context, outputPath)
+        ).rejects.toThrow(
+          new ExhaustOutputArgError(
+            WRITE_CSV_ERROR(outputPath, expectedMessage)
+          )
+        );
+      });
+
+      it('throws an error when output path is empty.', async () => {
+        const outputPath = '';
+
+        context.initialize = Object.assign({}, context.initialize, outputs);
+
+        expect.assertions(2);
+        try {
+          await exportCSVRaw.execute(tree, context, outputPath);
+        } catch (error) {
+          expect(error).toBeInstanceOf(ExhaustOutputArgError);
+          expect(error).toEqual(new ExhaustOutputArgError(OUTPUT_REQUIRED));
+        }
+      });
+    });
+  });
+});
