08cb44c3978a8eed947b1ce3e65ce7c903b3f6a0
Narek Hovhannisyan <narhovhannisian@gmail.com>
2024-07-05 14:18:04 +0400

    feat(src): init common types

---
+++ src/common/types/manifest.ts
@@ -0,0 +1,18 @@
+import {z} from 'zod';
+
+import {manifestSchema} from '../util/validations';
+
+export type Manifest = z.infer<typeof manifestSchema>;
+
+export type GlobalPlugins = Manifest['initialize']['plugins'];
+
+export type PluginOptions = GlobalPlugins[string];
+
+export type AggregationParams = Manifest['aggregation'];
+export type AggregationParamsSure = Extract<Manifest['aggregation'], {}>;
+
+export type Context = Omit<Manifest, 'tree'>;
+
+export type ContextWithExec = Omit<Manifest, 'tree'>;
+
+export type ManifestParameter = Extract<Manifest['params'], {}>[number];
