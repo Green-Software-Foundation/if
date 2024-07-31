fbeff3406ca5183e305075d0706e4124fedce882
Narek Hovhannisyan <narhovhannisian@gmail.com>
2024-07-31 19:11:04 +0400

    revert(types): drop isGroupBy interface

--- src/if-run/types/interface.ts
+++ src/if-run/types/interface.ts
@@ -4,6 +4,3 @@ export type PluginInterface = ExecutePlugin | GroupByPlugin;
 
 export const isExecute = (plugin: PluginInterface): plugin is ExecutePlugin =>
   (plugin as ExecutePlugin).metadata.kind === 'execute';
-
-export const isGroupBy = (plugin: PluginInterface): plugin is GroupByPlugin =>
-  (plugin as GroupByPlugin).metadata.kind === 'groupby';
