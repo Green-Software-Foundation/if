8b7014c9a8ba2b524b18d49afb286209f2cc428a
Narek Hovhannisyan <narhovhannisian@gmail.com>
2024-08-27 18:57:27 +0400

    feat(types): use aggregation from if core

--- src/if-run/types/aggregation.ts
+++ src/if-run/types/aggregation.ts
@@ -1,7 +1,7 @@
+import {AggregationMethodTypes} from '@grnsft/if-core/types';
+
 export type AggregationResult = Record<string, number>;
 
 export const AGGREGATION_TYPES = ['horizontal', 'vertical', 'both'] as const;
-export const AGGREGATION_METHODS = ['sum', 'avg', 'none'] as const;
 
-export type AggregationMethodTypes = 'sum' | 'avg' | 'none';
 export type AggregationMetric = Record<string, AggregationMethodTypes>;
