75de0ef60a7db3a108fb930bf208ce2a5664abb1
jmc <33655003+jmcook1186@users.noreply.github.com>
2024-10-16 14:26:15 +0100

    feat(src): update schema

--- src/if-attest/util/schema.ts
+++ src/if-attest/util/schema.ts
@@ -1,2 +1,2 @@
 export const SCHEMA =
-  'string start, string end, bytes32 hash, string if, bool verified, uint8 sci, string unit, uint8 energy, uint8 carbon, uint8 level, uint8 quality';
+  'string start, string end, bytes32 hash, string if, bool verified, uint64 sci, uint64 energy, uint64 carbon, uint8 level, uint8 quality, string functionalUnit';
