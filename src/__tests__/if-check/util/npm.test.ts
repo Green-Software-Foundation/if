63a6ee88bfba93865235d358fbc65b7b3fd71740
Narek Hovhannisyan <narhovhannisian@gmail.com>
2024-12-06 17:47:55 +0400

    test(util): fix if-check npm tests after merge

--- src/__tests__/if-check/util/npm.test.ts
+++ src/__tests__/if-check/util/npm.test.ts
@@ -35,21 +35,6 @@ jest.mock('child_process', () => {
             )
           ).toBeTruthy();
           break;
-        case 'if-check-cwd':
-          expect(param).toEqual(
-            "npm run if-env  -- -m ./src/__mocks__/mock-manifest.yaml -c && npm run if-run  -- -m ./src/__mocks__/mock-manifest.yaml -o src/__mocks__/re-mock-manifest &&  node -p 'Boolean(process.stdout.isTTY)' | npm run if-diff  -- -s src/__mocks__/re-mock-manifest.yaml -t ./src/__mocks__/mock-manifest.yaml"
-          );
-          break;
-        case 'if-check-global':
-          expect(param).toEqual(
-            "if-env  -m ./src/__mocks__/mock-manifest.yaml && if-run  -m ./src/__mocks__/mock-manifest.yaml -o src/__mocks__/re-mock-manifest &&  node -p 'Boolean(process.stdout.isTTY)' | if-diff  -s src/__mocks__/re-mock-manifest.yaml -t ./src/__mocks__/mock-manifest.yaml"
-          );
-          break;
-        case 'if-check-prefix':
-          expect(param).toEqual(
-            "if-env --prefix=.. -m ./src/__mocks__/mock-manifest.yaml && if-run --prefix=.. -m ./src/__mocks__/mock-manifest.yaml -o src/__mocks__/re-mock-manifest &&  node -p 'Boolean(process.stdout.isTTY)' | if-diff --prefix=.. -s src/__mocks__/re-mock-manifest.yaml -t ./src/__mocks__/mock-manifest.yaml"
-          );
-          break;
       }
       return;
     },
@@ -68,48 +53,6 @@ describe('if-check/util/npm: ', () => {
       await executeCommands(manifest, false);
 
       expect.assertions(6);
-      expect(logSpy).toHaveBeenCalledWith(
-        '✔ if-check successfully verified mock-manifest.yaml\n'
-      );
-    });
-
-    it('successfully executes with cwd command.', async () => {
-      process.env.NPM_INSTALL = 'if-check-cwd';
-      const manifest = './src/__mocks__/mock-manifest.yaml';
-      const logSpy = jest.spyOn(global.console, 'log');
-
-      await executeCommands(manifest, true);
-
-      expect.assertions(2);
-      expect(logSpy).toHaveBeenCalledWith(
-        '✔ if-check successfully verified mock-manifest.yaml\n'
-      );
-    });
-
-    it('successfully executes with correct commands when is running from global.', async () => {
-      process.env.npm_config_global = 'true';
-      process.env.NPM_INSTALL = 'if-check-global';
-      const manifest = './src/__mocks__/mock-manifest.yaml';
-      const logSpy = jest.spyOn(global.console, 'log');
-
-      await executeCommands(manifest, false);
-
-      expect.assertions(2);
-      expect(logSpy).toHaveBeenCalledWith(
-        '✔ if-check successfully verified mock-manifest.yaml\n'
-      );
-    });
-
-    it('successfully executes with correct commands when CURRENT_DIR is provided.', async () => {
-      process.env.CURRENT_DIR = './mock-path';
-      process.env.npm_config_global = 'true';
-      process.env.NPM_INSTALL = 'if-check-prefix';
-      const manifest = './src/__mocks__/mock-manifest.yaml';
-      const logSpy = jest.spyOn(global.console, 'log');
-
-      await executeCommands(manifest, false);
-
-      expect.assertions(2);
       expect(logSpy).toHaveBeenCalledWith(
         '✔ if-check successfully verified mock-manifest.yaml\n'
       );
